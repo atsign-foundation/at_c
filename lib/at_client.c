8c5781d152ae308b69e93ce075da8e0818dbed34
xavierchanth <xchanthavong@gmail.com>
2023-02-02 17:29:38 -0500

    chore: Increase compiler strictness

--- lib/at_client.c
+++ lib/at_client.c
@@ -10,7 +10,7 @@ int do_something(int a)
 int do_md5()
 {
   // Used as a temporary test for ensuring that mbedtls is linked correctly
-  const unsigned char *in = (unsigned char *)("Hello, World!");
+  const unsigned char *in = (const unsigned char *)("Hello, World!");
   unsigned char out[500];
   int retval = mbedtls_md5(in, 13, out);
   return retval;
