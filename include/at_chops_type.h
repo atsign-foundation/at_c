65171f18054498daa49d20fe3c90e3912823c568
xavierchanth <xchanthavong@gmail.com>
2023-02-21 17:43:50 -0500

    chore: cleanup unused code

--- include/at_chops_type.h
+++ include/at_chops_type.h
@@ -10,30 +10,4 @@ namespace AtChopsType
     size_t iv_len;
     DartType::Uint8List iv;
   };
-
-  // struct AtEncryptionMetadata
-  // {
-  //   size_t atEncryptionAlgorithmLen;
-  //   DartType::String atEncryptionAlgorithm;
-
-  //   size_t keyNameLen;
-  //   DartType::String keyName;
-
-  //   EncryptionKeyType encryptionKeyType;
-  //   InitialisationVector iv;
-  // };
-
-  // struct AtEncrytionResult
-  // {
-  //   size_t messageLen;
-  //   DartType::String message;
-
-  //   AtEncryptionResultType type;
-  // };
-
-  // struct AtSigningResult
-  // {
-  //   size_t message_len;
-  //   DartType::String message;
-  // };
 }
