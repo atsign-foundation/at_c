b0a0adfa5e49bb69563b592cb8495b8a5499f3c3
xavierchanth <xchanthavong@gmail.com>
2023-02-22 17:24:59 -0500

    docs: Document at_chops function signatures

--- include/at_chops.h
+++ include/at_chops.h
@@ -28,28 +28,320 @@ typedef struct
 } AtEncryptionKey;
 
 // Encryption and Decryption - AES
-extern void decryptBytes_AES(uint8_t *data, size_t dataLen, AtEncryptionKey *key, InitialisationVector *iv);
-extern void decryptString_AES(char *data, size_t dataLen, AtEncryptionKey *key, InitialisationVector *iv);
-extern void encryptBytes_AES(uint8_t *data, size_t dataLen, AtEncryptionKey *key);
-extern void encryptString_AES(char *data, size_t dataLen, AtEncryptionKey *key);
+
+/**
+ * \brief          Decrypts a buffer using AES
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ * \param iv       initialisation vector
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void decryptBytes_AES(uint8_t *dst, const size_t dlen, size_t *olen,
+                             const uint8_t *src, const size_t slen,
+                             const AtEncryptionKey *key,
+                             const InitialisationVector *iv);
+/**
+ * \brief          Decrypts a buffer using AES
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ * \param iv       initialisation vector
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void decryptString_AES(char *dst, const size_t dlen, size_t *olen,
+                              const char *src, const size_t slen,
+                              const AtEncryptionKey *key,
+                              const InitialisationVector *iv);
+/**
+ * \brief          Encrypts a buffer using AES
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void encryptBytes_AES(uint8_t *dst, const size_t dlen, size_t *olen,
+                             const uint8_t *src, const size_t slen,
+                             const AtEncryptionKey *key);
+/**
+ * \brief          Encrypts a buffer using AES
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void encryptString_AES(char *dst, const size_t dlen, size_t *olen,
+                              const char *src, const size_t slen,
+                              const AtEncryptionKey *key);
 
 // Encryption and Decryption - ECC
-extern void decryptBytes_ECC(uint8_t *data, size_t dataLen, AtEncryptionKey *key, InitialisationVector *iv);
-extern void decryptString_ECC(char *data, size_t dataLen, AtEncryptionKey *key, InitialisationVector *iv);
-extern void encryptBytes_ECC(uint8_t *data, size_t dataLen, AtEncryptionKey *key);
-extern void encryptString_ECC(char *data, size_t dataLen, AtEncryptionKey *key);
+
+/**
+ * \brief          Decrypts a buffer using ECC
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ * \param iv       initialisation vector
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void decryptBytes_ECC(uint8_t *dst, const const size_t dlen, size_t *olen,
+                             const uint8_t *src, const size_t slen,
+                             const AtEncryptionKey *key,
+                             const InitialisationVector *iv);
+/**
+ * \brief          Decrypts a buffer using ECC
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ * \param iv       initialisation vector
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void decryptString_ECC(char *dst, const size_t dlen, size_t *olen,
+                              const char *src, const size_t slen,
+                              const AtEncryptionKey *key,
+                              const InitialisationVector *iv);
+/**
+ * \brief          Encrypts a buffer using ECC
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void encryptBytes_ECC(uint8_t *dst, const size_t dlen, size_t *olen,
+                             const uint8_t *src, const size_t slen,
+                             const AtEncryptionKey *key);
+/**
+ * \brief          Encrypts a buffer using ECC
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void encryptString_ECC(char *dst, const size_t dlen, size_t *olen,
+                              const char *src, const size_t slen,
+                              const AtEncryptionKey *key);
 
 // Encryption and Decryption - RSA
-extern void decryptBytes_RSA(uint8_t *data, size_t dataLen, AtEncryptionKey *key);
-extern void decryptString_RSA(char *data, size_t dataLen, AtEncryptionKey *key);
-extern void encryptBytes_RSA(uint8_t *data, size_t dataLen, AtEncryptionKey *key);
-extern void encryptString_RSA(char *data, size_t dataLen, AtEncryptionKey *key);
+
+/**
+ * \brief          Decrypts a buffer using RSA
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void decryptBytes_RSA(uint8_t *dst, const size_t dlen, size_t *olen,
+                             const uint8_t *src, const size_t slen,
+                             const AtEncryptionKey *key);
+/**
+ * \brief          Decrypts a buffer using RSA
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void decryptString_RSA(char *dst, const size_t dlen, size_t *olen,
+                              const char *src, const size_t slen,
+                              const AtEncryptionKey *key);
+/**
+ * \brief          Encrypts a buffer using RSA
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void encryptBytes_RSA(uint8_t *dst, const size_t dlen, size_t *olen,
+                             const uint8_t *src, const size_t slen,
+                             const AtEncryptionKey *key);
+/**
+ * \brief          Encrypts a buffer using RSA
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void encryptString_RSA(char *dst, const size_t dlen, size_t *olen,
+                              const char *src, const size_t slen,
+                              const AtEncryptionKey *key);
 
 // Hashing
-extern char *hash_SHA512(uint8_t *signedData, size_t signedDataLen);
+
+/**
+ * \brief          Computes the SHA512 hash of a buffer
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void hash_SHA512(uint8_t *dst, const size_t dlen, size_t *olen,
+                        const uint8_t *src, const size_t slen);
 
 // Signing and Verification - RSA/SHA256
-extern void signBytes_RSA_SHA256(uint8_t data, size_t dataLen, AtEncryptionKey *key);
-extern void signString_RSA_SHA256(char *data, size_t dataLen, AtEncryptionKey *key);
-extern void verifySignatureBytes_RSA_SHA256(uint8_t *data, size_t dataLen, uint8_t *signature, size_t signatureLen, AtEncryptionKey *key);
-extern void verifySignatureString_RSA_SHA256(char *data, size_t dataLen, char *signature, size_t signatureLen, AtEncryptionKey *key);
+
+/**
+ * \brief          Signs a buffer using RSA and SHA256
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void signBytes_RSA_SHA256(uint8_t *dst, const size_t dlen, size_t *olen,
+                                 const uint8_t *src, const size_t slen,
+                                 const AtEncryptionKey *key);
+/**
+ * \brief          Signs a buffer using RSA and SHA256
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data in the source buffer
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+extern void signString_RSA_SHA256(char *dst, const size_t dlen, size_t *olen,
+                                  const char *src, const size_t slen,
+                                  const AtEncryptionKey *key);
+/**
+ * \brief          Verifies data against a signature using RSA and SHA256
+ *
+ * \param data     data (can be NULL for checking size)
+ * \param dlen     size of the data
+ * \param sign     signature
+ * \param slen     size of the signature
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ */
+extern bool verifyBytes_RSA_SHA256(const uint8_t *data, const size_t dlen,
+                                   const uint8_t *sign, const size_t slen,
+                                   const AtEncryptionKey *key);
+/**
+ * \brief          Verifies data against a signature using RSA and SHA256
+ *
+ * \param data     data (can be NULL for checking size)
+ * \param dlen     size of the data
+ * \param sign     signature
+ * \param slen     size of the signature
+ * \param key      encryption key
+ *
+ * \return         0 if successful, or an error code
+ */
+extern bool verifyString_RSA_SHA256(const char *data, const size_t dlen,
+                                    const char *sign, const size_t slen,
+                                    const AtEncryptionKey *key);
