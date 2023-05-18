b3661314eead6958e5e4b02bafb35590d3613f10
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-05-17 22:03:06 -0400

    feat: `at_chops` mbedtls desktop base64

---
+++ src/at_chops/base64.c
@@ -0,0 +1,34 @@
+#ifdef BUILD_MBEDTLS
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stddef.h>
+#include <mbedtls/base64.h>
+
+int atchops_base64_encode(unsigned char *dst, size_t dstlen, size_t *writtenlen, const unsigned char *src, const size_t srclen)
+{
+    return mbedtls_base64_encode(
+        dst,
+        dstlen,
+        writtenlen,
+        src,
+        srclen);
+}
+
+int atchops_base64_decode(unsigned char *dst, size_t dstlen, size_t *writtenlen, const unsigned char *src, const size_t srclen)
+{
+    return mbedtls_base64_decode(
+        dst,
+        dstlen,
+        writtenlen,
+        src,
+        srclen);
+}
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
