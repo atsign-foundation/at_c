814256c81631f89afe23261c5ce279d7f016dff8
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-06-01 20:21:38 -0400

    docs: base64 documentation on return

--- include/at_chops/base64.h
+++ include/at_chops/base64.h
@@ -15,7 +15,7 @@ extern "C"
  * @param written_len the length of the result after operation
  * @param src the plain text string to encode
  * @param src_len the size of the plain text string to encode
- * @return int `0,` if successful
+ * @return int `0,` if successful, 0 if successful, MBEDTLS_ERR_BASE64_BUFFER_TOO_SMALL, or MBEDTLS_ERR_BASE64_INVALID_CHARACTER if the input data is not correct. *olen is always updated to reflect the amount of data that has (or would have) been written.
  */
 int atchops_base64_encode(unsigned char *dst, size_t dstlen, size_t *writtenlen, const unsigned char *src, const size_t srclen);
 
