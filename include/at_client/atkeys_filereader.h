da11a7af791687cf6168a4227031dbe47bd2d995
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-07-31 10:11:36 -0400

    docs: usage example atkeys_filereader.h

--- include/at_client/atkeys_filereader.h
+++ include/at_client/atkeys_filereader.h
@@ -32,3 +32,28 @@ void atclient_atkeysfile_init(atclient_atkeysfile *atkeysfile);
 int atclient_atkeysfile_read(const char *path, atclient_atkeysfile *atkeysfile);
 int atclient_atkeysfile_write(const char *path, const char *atsign, atclient_atkeysfile *atkeysfile);
 void atclient_atkeysfile_free(atclient_atkeysfile *atkeysfile);
+
+/**
+ * Usage example
+ * atclient_atkeysfile atkeysfile;
+ * atclient_atkeysfile_init(&atkeysfile);
+ * printf("done init...\n")
+ *
+    ret = atclient_atkeysfile_read(path, &atkeysfile);
+    if (ret != 0)
+    {
+        goto exit;
+    }
+
+    printf("done read...\n");
+    printf("aes_pkam_public_key: %s\n", atkeysfile.aes_pkam_public_key->key);
+    printf("aes_pkam_private_key: %s\n", atkeysfile.aes_pkam_private_key->key);
+    printf("aes_encrypt_public_key: %s\n", atkeysfile.aes_encrypt_public_key->key);
+    printf("aes_encrypt_private_key: %s\n", atkeysfile.aes_encrypt_private_key->key);
+    printf("self_encryption_key: %s\n", atkeysfile.self_encryption_key->key);
+
+    printf("writing...\n");
+
+    ret = atclient_atkeysfile_write("/Users/jeremytubongbanua/.atsign/temp/@smoothalligator_key.atKeys", ATSIGN, &atkeysfile);
+ *
+ */
