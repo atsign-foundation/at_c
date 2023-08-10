cac469e5c2c482a465ad72bbaa7ac25e93e5a22b
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-08-10 00:00:31 -0400

    fix: add stddef

--- packages/atchops/include/atchops/byteutil.h
+++ packages/atchops/include/atchops/byteutil.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stddef.h>
+
 void printx(unsigned char *data, size_t len);
 
 void copy(unsigned char *dst, unsigned char *src, size_t len);
--- packages/atchops/include/atchops/rsa.h
+++ packages/atchops/include/atchops/rsa.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stddef.h>
 #include <ctype.h>
 #include "sha.h"
 
