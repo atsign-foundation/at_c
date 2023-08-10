31f208fa48900247ed290d60eeee0f52158e36af
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-08-09 20:59:20 -0400

    refactor: bin instead of install dir

--- packages/repl/run.sh
+++ packages/repl/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eu
-sudo rm -rf build
 rm -f build/CMakeCache.txt
 cmake -S . -B build
-sudo cmake --build build --target install/local
-./install/repl
+sudo cmake --build build --target install
+./bin/repl
