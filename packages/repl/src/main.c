6ab06acf65b85866e9e27b7bc185f5fc81bc47a3
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-08-14 17:26:56 -0400

    feat: adaptive

--- packages/repl/src/main.c
+++ packages/repl/src/main.c
@@ -5,44 +5,76 @@
 #include <atclient/connection.h>
 #include <atclient/atkeys_filereader.h>
 
+#define ATSIGN "@jeremy_0"
+// #define ATKEYS_FILE_PATH "/Users/jeremytubongbanua/.atsign/keys/@jeremy_0_key.atKeys"
+#define ATKEYS_FILE_PATH "./@jeremy_0_key.atKeys"
+
+static void *without_at_symbol(char *atsign, char *buf)
+{
+    int i = 0;
+    while (atsign[i] != '\0')
+    {
+        buf[i] = atsign[i + 1];
+        i++;
+    }
+    buf[i] = '\0';
+    return buf;
+}
+
 int main()
 {
-
     int ret = 1;
 
-    // initialize buffer to use throughout program
+    // 1. initialize buffer to use throughout program
 
     size_t recvlen = 32768;
     unsigned char *recv = malloc(sizeof(unsigned char) * recvlen);
-    size_t *olen = malloc(sizeof(size_t));
+    size_t olen = 0;
 
-    // initialize atkeys
+    // 2. initialize atkeys, `atkeysfile` is now a struct that holds read atkeys
 
-    const char *keyspath = "/Users/jeremytubongbanua/.atsign/keys/@jeremy_0_key.atKeys";
+
+    const char *keyspath = ATKEYS_FILE_PATH;
+    printf("Reading keys from \"%s\"...\n", keyspath);
 
     atclient_atkeysfile atkeysfile;
     atclient_atkeysfile_init(&atkeysfile);
-    atclient_atkeysfile_read(keyspath, &atkeysfile);
+    ret = atclient_atkeysfile_read(keyspath, &atkeysfile);
+    if(ret != 0)
+    {
+        printf("Error reading keys from \"%s\"\n", keyspath);
+        goto exit;
+    }
+    printf("Done reading...\n");
 
-    // connect to root and find secondary address
+    // 3. connect to root and find secondary address
 
+    // 3a. establish connection to root
     atclient_connection_ctx root_connection;
     atclient_connection_init(&root_connection);
-    atclient_connection_connect(&root_connection, HOST, PORT);
+    atclient_connection_connect(&root_connection, "root.atsign.org", 64);
+    printf("Connected to root\n");
 
-    unsigned char *src = "jeremy_0\r\n";
-    size_t srclen = strlen(src);
+    // 3b. send atsign without @ symbol to root
 
-    atclient_connection_send(&root_connection, recv, recvlen, olen, src, srclen);
+    unsigned char *atsign_without_at = malloc(sizeof(unsigned char) * 100);
+    memset(atsign_without_at, 0, 100);
+    without_at_symbol(ATSIGN, atsign_without_at);
+    strcat(atsign_without_at, "\r\n");
+    size_t atsign_without_atlen = strlen(atsign_without_at);
 
-    printf("\"%.*s\"\n", (int)*olen, recv);
+    printf("Sending to root: \"%s\"\n", atsign_without_at);
+    ret = atclient_connection_send(&root_connection, recv, recvlen, &olen, atsign_without_at, atsign_without_atlen);
+    printf("Received from root: \"%.*s\"\n", (int) olen, recv);
+
+    // 3c. parse secondary address
 
     const size_t secondary_len = 100;
     char *secondary_host = malloc(sizeof(char) * secondary_len);
     char *secondary_port = malloc(sizeof(char) * secondary_len);
 
     int i = 0, c;
-    while ((c = recv[i]) != ':' && i < *olen)
+    while ((c = recv[i]) != ':' && i < olen)
     {
         secondary_host[i] = c;
         i++;
@@ -50,7 +82,7 @@ int main()
     secondary_host[i] = '\0';
     i++;
     int j = 0;
-    while ((c = recv[i]) != '\0' && i < *olen)
+    while ((c = recv[i]) != '\0' && i < olen)
     {
         secondary_port[j] = c;
         i++;
@@ -60,26 +92,33 @@ int main()
     printf("secondary_host: %s\n", secondary_host);
     printf("secondary_port: %s\n", secondary_port);
 
-    // connect to secondary
+    // 4. connect to secondary
 
+    // 4a. establish secondary connection
     atclient_connection_ctx secondary_connection;
     atclient_connection_init(&secondary_connection);
     atclient_connection_connect(&secondary_connection, secondary_host, atoi(secondary_port));
 
-    // send from request
+    // 4b. send from request
 
     memset(recv, 0, recvlen);
-    const unsigned char *from_command = "from:@jeremy_0\r\n";
-    const size_t from_command_len = strlen(from_command);
-    atclient_connection_send(&secondary_connection, recv, recvlen, olen, from_command, from_command_len);
+    const unsigned char *from_command = malloc(sizeof(unsigned char) * 1024);
+    memset(from_command, 0, 1024);
+    strcat(from_command, "from:");
+    strcat(from_command, ATSIGN);
+    strcat(from_command, "\r\n");
+    const size_t from_commandlen = strlen(from_command);
+    atclient_connection_send(&secondary_connection, recv, recvlen, &olen, from_command, from_commandlen);
+    printf("Sent: \"%.*s\" | Received: \"%.*s\"\n", (int) from_commandlen, from_command, (int) olen, recv);
 
-    const size_t from_response_len = 1024;
-    unsigned char *from_response = malloc(sizeof(unsigned char) * from_response_len);
-    memset(from_response, 0, from_response_len);
+    // 4c. parse from response, store data after the `data:` prefix
+    const size_t from_responselen = 1024;
+    unsigned char *from_response = malloc(sizeof(unsigned char) * from_responselen);
+    memset(from_response, 0, from_responselen);
 
     int in = 0;
     j = 0;
-    for (int i = 0; i < *olen; i++)
+    for (int i = 0; i < olen; i++)
     {
         char c = recv[i];
         if (in == 1)
@@ -92,7 +131,7 @@ int main()
         }
     }
 
-    printf("from_response: %s\n", from_response);
+    printf("from_response: %.*s\n", (int) olen, (const char *) from_response);
 
     // get pkam private key
 
@@ -135,7 +174,6 @@ int main()
     size_t pkamcommandlen = 32768;
     unsigned char *pkamcommand = malloc(sizeof(unsigned char) * pkamcommandlen);
     memset(pkamcommand, 0, pkamcommandlen);
-    memset(pkamcommand, 0, pkamcommandlen);
 
     strcat(pkamcommand, "pkam:");
     strncat(pkamcommand, signature, signaturelen);
@@ -143,9 +181,9 @@ int main()
 
     printf("pkam command: \"%s\"\n", pkamcommand);
 
-    atclient_connection_send(&secondary_connection, recv, recvlen, olen, pkamcommand, strlen(pkamcommand));
+    atclient_connection_send(&secondary_connection, recv, recvlen, &olen, pkamcommand, strlen(pkamcommand));
 
-    printf("\"%.*s\"\n", (int)*olen, recv);
+    printf("\"%.*s\"\n", (int) olen, recv);
 
     size_t commandlen = 32768;
     unsigned char *command = malloc(sizeof(unsigned char) * commandlen);
@@ -157,9 +195,9 @@ int main()
         {
             break;
         }
-        atclient_connection_send(&secondary_connection, recv, recvlen, olen, command, strlen(command));
+        atclient_connection_send(&secondary_connection, recv, recvlen, &olen, command, strlen(command));
         memset(command, 0, commandlen);
-        printf("\nrecv: \"%.*s\"\n\n", *olen, recv);
+        printf("\nrecv: \"%.*s\"\n\n", olen, recv);
     }
 
     goto exit;
