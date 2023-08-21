3f6760044abc3c7f607758251d68455c5cb78481
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-08-21 19:48:54 -0400

    chore: deleted my shell scripts

--- packages/atchops/install-here.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eu
-sudo rm -f build/CMakeCache.txt
-sudo rm -rf install
-mkdir -p install
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX=./install -DATCHOPS_FETCH_MBEDTLS=ON
-cmake --build build --target install/local
--- packages/atchops/run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-# sudo rm -rf build
-sudo cmake -S . -B build -DATCHOPS_BUILD_TESTS=ON -DATCHOPS_FETCH_MBEDTLS=ON -DCMAKE_INSTALL_PREFIX=./install
-sudo rm -rf install
-sudo cmake --build build --target all
-cd build/tests
-sudo ctest --output-on-failure -V
-cd ../..
