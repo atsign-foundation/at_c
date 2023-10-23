74625554e6fbab6d68818ffc2d30fa42fcf16b26
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-10-23 15:19:18 -0400

    refactor: iv is not const

--- packages/atchops/include/atchops/aes_ctr.h
+++ packages/atchops/include/atchops/aes_ctr.h
@@ -10,7 +10,7 @@ int atchops_aes_ctr_encrypt(
     const char *keybase64,
     const unsigned long keybase64len,
     const AESKeySize keybits,
-    const unsigned char *iv, // always 16 bytes
+    unsigned char *iv, // always 16 bytes long
     const unsigned char *plaintext,
     const unsigned long plaintextlen,
     unsigned char *ciphertextbase64,
@@ -21,7 +21,7 @@ int atchops_aes_ctr_decrypt(
     const char *keybase64,
     const unsigned long keybase64len,
     const AESKeySize keybits,
-    const unsigned char *iv, // always 16 bytes
+    unsigned char *iv, // always 16 bytes long
     const unsigned char *ciphertextbase64,
     const unsigned long ciphertextbase64len,
     unsigned char *plaintext,
