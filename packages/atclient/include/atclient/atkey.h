691a6170f933b5d1014806cb20f53c55ce892dd5
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-10-30 21:49:21 -0400

    feat: atkey

--- packages/atclient/include/atclient/atkey.h
+++ packages/atclient/include/atclient/atkey.h
@@ -1,41 +1,107 @@
 #ifndef ATKEY_H
 #define ATKEY_H
 
+#include "atclient/atsign.h"
+#include "atclient/metadata.h"
+
 typedef enum atclient_atkey_type
 {
-    PUBLICKEY = 0,
-    SELFKEY,
-    SHAREDKEY,
+	UNKNOWN = 0,
+	PUBLICKEY,
+	SELFKEY,
+	SHAREDKEY,
 } atclient_atkey_type;
 
 typedef struct atclient_atkey
 {
-    unsigned long namelen;
-    char *namestr;
-    unsigned long nameolen;
+	unsigned long namelen;
+	char *namestr;
+	unsigned long nameolen;
 
-    unsigned long namespacelen;
-    char *namespacestr;
-    unsigned long namespaceolen;
+	unsigned long namespacelen;
+	char *namespacestr;
+	unsigned long namespaceolen;
 
-    unsigned long sharedwithlen;
-    char *sharedwithstr;
-    unsigned long sharedwitholen;
+	unsigned long sharedwithlen;
+	char *sharedwithstr;
+	unsigned long sharedwitholen;
 
-    unsigned long sharedbylen;
-    char *sharedbystr;
-    unsigned long sharedbyolen;
+	unsigned long sharedbylen;
+	char *sharedbystr;
+	unsigned long sharedbyolen;
 
-    atclient_atkey_type atkeytype;
+	atclient_atkey_type atkeytype;
+
+	atclient_atkey_metadata metadata;
 
 } atclient_atkey;
 
+/**
+ * @brief Initialize an atkey struct. This function should be called before any other atkey functions.
+ *
+ * @param atkey the atkey struct to initialize
+ */
 void atclient_atkey_init(atclient_atkey *atkey);
+
+/**
+ * @brief free an atkey struct. This function should be called at the end of an atkey's life
+ *
+ * @param atkey the atkey struct to free
+ */
 void atclient_atkey_free(atclient_atkey *atkey);
-int atclient_atkey_create_publickey(atclient_atkey *atkey, const char *name, const char *sharedby, const char *namespacestr);
-int atclient_atkey_create_selfkey(atclient_atkey *atkey, char *name, char *sharedby, char *namespacestr);
-int atclient_atkey_create_sharedkey(atclient_atkey *atkey, char *name, char *sharedby, char *namespacestr, char *sharedwith);
+
+/**
+ * @brief populate an atkey struct given a null terminated string. Be sure to call the atclient_atkey_init function before calling this function.
+ *
+ * @param atkey the atkey struct to populate
+ * @param atkeystr the atkeystr to derive from (e.g. 'public:name.wavi@alice')
+ * @return int 0 on success, that a struct was able to be created from the string. (the string followed proper key nomenclature)
+ */
 int atclient_atkey_create_from_string(atclient_atkey *atkey, const char *atkeystr);
+
+/**
+ * @brief convert an atkey struct to its string format
+ *
+ * @param atkey atkey struct to read
+ * @param atkeystr buffer to write to
+ * @param atkeystrlen buffer allocated size
+ * @param atkeystrolen the written (output) length of the atkeystr
+ * @return int 0 on success
+ */
 int atclient_atkey_to_string(atclient_atkey atkey, char *atkeystr, unsigned long *atkeystrlen, unsigned long atkeystrolen);
+
+/**
+ * @brief Populate an atkey struct representing a PublicKey AtKey with null terminated strings. An example of a Public AtKey would be 'public:name.namespace@alice'. Public AtKeys typically hold unencrypted values and can be seen by unauthenticated atsigns. Be sure to call the atclient_atkey_init function before calling this function.
+ *
+ * @param atkey the atkey struct to populate
+ * @param name the name of the atkey, e.g.: "name"
+ * @param sharedby the sharedby (creator/pkam authenticated atsign) of the atkey, e.g.: "@alice"
+ * @param namespacestr the namespace of your application, e.g. "banking_app" (NULLABLE)
+ * @return int 0 on success
+ */
+int atclient_atkey_create_publickey(atclient_atkey *atkey, const char *name, const atclient_atsign sharedby, const char *namespacestr);
+
+/**
+ * @brief Populate an atkey struct representing a SelfKey AtKey with null terminated strings. An example of a SelfKey AtKey would be 'name.namespace@alice'. SelfKeys can only be accessible by the sharedby (creator) atsign. Be sure to call the atclient_atkey_init function before calling this function.
+ *
+ * @param atkey the atkey struct to populate
+ * @param name the name of the atkey, e.g.: "name"
+ * @param sharedby the sharedby (creator/pkam authenticated atsign) of the atkey, e.g.: "@alice"
+ * @param namespacestr the namespace of your application, e.g. "banking_app" (NULLABLE)
+ * @return int 0 on success
+ */
+int atclient_atkey_create_selfkey(atclient_atkey *atkey, char *name, const atclient_atsign sharedby, char *namespacestr);
+
+/**
+ * @brief Populate an atkey struct representing a SharedKey AtKey given null terminated strings. An example of a SharedKey AtKey would be '@sharedwith:name.namesapce@sharedby'. SharedKeys can only be accessible by the sharedwith and sharedby atsigns, as they are encrypted with a shared AES key which is encrypted with the each of their RSA keys. Be sure to call the atclient_atkey_init function before calling this function.
+ *
+ * @param atkey the atkey struct to populate
+ * @param name name of your key, e.g. "name"
+ * @param sharedby the shared by atsign, e.g. "@alice"
+ * @param sharedwith the sharedwith atsign, atsign you are going to share it with, e.g. "@bob"
+ * @param namespacestr the namespace of your application, e.g. "banking_app" (NULLABLE)
+ * @return int 0 on success
+ */
+int atclient_atkey_create_sharedkey(atclient_atkey *atkey, char *name, const atclient_atsign sharedby, const atclient_atsign sharedwith, char *namespacestr);
 
 #endif
--- packages/atclient/src/atkey.c
+++ packages/atclient/src/atkey.c
@@ -1,39 +1,88 @@
 #include <stdlib.h>
 #include <string.h>
 #include "atclient/atkey.h"
+#include "atclient/metadata.h"
+
+#define ATKEY_GENERAL_BUFFER_SIZE 4096 // sufficient memory for keyName, namespace, sharedWith, and sharedBy strings
 
 void atclient_atkey_init(atclient_atkey *atkey)
 {
     memset(atkey, 0, sizeof(atclient_atkey));
-    return; // TODO: implement
+    atkey->namelen = ATKEY_GENERAL_BUFFER_SIZE;
+    atkey->namestr = (char *)malloc(sizeof(char) * atkey->namelen);
+    atkey->nameolen = 0;
+
+    atkey->namespacelen = ATKEY_GENERAL_BUFFER_SIZE;
+    atkey->namespacestr = (char *)malloc(sizeof(char) * atkey->namespacelen);
+    atkey->namespaceolen = 0;
+
+    atkey->sharedwithlen = ATKEY_GENERAL_BUFFER_SIZE;
+    atkey->sharedwithstr = (char *)malloc(sizeof(char) * atkey->sharedwithlen);
+    atkey->sharedwitholen = 0;
+
+    atkey->sharedbylen = ATKEY_GENERAL_BUFFER_SIZE;
+    atkey->sharedbystr = (char *)malloc(sizeof(char) * atkey->sharedbylen);
+    atkey->sharedbyolen = 0;
+
+    atclient_atkey_metadata_init(&(atkey->metadata));
 }
 
 void atclient_atkey_free(atclient_atkey *atkey)
 {
-    return; // TODO: implement
-}
-
-int atclient_atkey_create_publickey(atclient_atkey *atkey, const char *name, const char *sharedby, const char *namespacestr)
-{
-    return 1; // TODO: implement
-}
-
-int atclient_atkey_create_selfkey(atclient_atkey *atkey, char *name, char *sharedby, char *namespacestr)
-{
-    return 1; // TODO: implement
-}
-
-int atclient_atkey_create_sharedkey(atclient_atkey *atkey, char *name, char *sharedby, char *namespacestr, char *sharedwith)
-{
-    return 1; // TODO: implement
+    free(atkey->namestr);
+    free(atkey->namespacestr);
+    free(atkey->sharedwithstr);
+    free(atkey->sharedbystr);
 }
 
 int atclient_atkey_create_from_string(atclient_atkey *atkey, const char *atkeystr)
 {
-    return 1; // TODO: implement
-}
+    // 6 scenarios:
+    // 1. PublicKey:            "public:name.wavi@smoothalligator"
+    //      name == "name"
+    //      sharedby = "smoothalligator"
+    //      sharedwith = NULL
+    //      namespace = "wavi"
+    //      type = "public"
+    //      cached = false
+    // 2. PublicKey (cached):   "cached:public:name.wavi@smoothalligator"
+    //      name == "name"
+    //      sharedby = "smoothalligator"
+    //      sharedwith = NULL
+    //      namespace = "wavi"
+    //      type = "public"
+    //      cached = true
+    // 3. SharedKey:            "@foo:name.wavi@bar"
+    //      name == "name"
+    //      sharedby = "bar"
+    //      sharedwith = "foo"
+    //      namespace = "wavi"
+    //      cached = false
+    // 4. SharedKey (cached):   "cached:@bar:name.wavi@foo"
+    //      name == "name"
+    //      sharedby = "foo"
+    //      sharedwith = "bar"
+    //      namespace = "wavi"
+    //      cached = true
+    // 5. PrivateHiddenKey:     "_latestnotificationid.wavi@smoothalligator"
+    //      name == "_latestnotificationid"
+    //      sharedby = "smoothalligator"
+    //      namespace = "wavi"
+    //      cached = false
+    // 6. SelfKey:              "name.wavi@smoothalligator"
+    //      name == "name"
+    //      sharedby = "smoothalligator"
+    //      sharedwith = NULL
+    //      namespace = "wavi"
+    //      cached = false
+    // more scenarios
+    // 7. No Namespace, SelfKey:
+    // "name@smoothalligator"
+    //      name == "name"
+    //      sharedby = "smoothalligator"
+    //      sharedwith = NULL
+    //      namespace = NULL
+    //      cached = false
 
-int atclient_atkey_to_string(atclient_atkey atkey, char *atkeystr, unsigned long *atkeystrlen, unsigned long atkeystrolen)
-{
     return 1; // TODO: implement
 }
