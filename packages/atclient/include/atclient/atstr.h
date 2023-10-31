3969b9f1d8ffd6a8bd14f9a3416d3b9369b34fe2
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-10-30 22:57:17 -0400

    feat: delete atsign and made atstr

---
+++ packages/atclient/include/atclient/atstr.h
@@ -0,0 +1,29 @@
+#ifndef ATSTR_H
+#define ATSTR_H
+
+/**
+ * @brief Represents a string that is allocated on the heap
+ */
+typedef struct atclient_atstr
+{
+    unsigned long len;  // buffer length
+    char *str;          // string
+    unsigned long olen; // output length (length of string)
+} atclient_atstr;
+
+/**
+ * @brief Initialize an atstr
+ *
+ * @param atstr pointer to atstr to initialize
+ * @param bufferlen length of buffer to allocate in bytes (recommended to use a number that is a power of 2)
+ */
+void atclient_atstr_init(atclient_atstr *atstr, unsigned long bufferlen);
+
+/**
+ * @brief Free an atstr
+ *
+ * @param atstr pointer to atstr to free from the heap
+ */
+void atclient_atstr_free(atclient_atstr *atstr);
+
+#endif
---
+++ packages/atclient/src/atstr.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdlib.h>
+#include "atclient/atstr.h"
+
+void atclient_atstr_init(atclient_atstr *atstr, unsigned long bufferlen)
+{
+    memset(atstr, 0, sizeof(atclient_atstr));
+    atstr->len = bufferlen;
+    atstr->str = (char *)malloc(sizeof(char) * atstr->len);
+    atstr->olen = 0;
+}
+
+void atclient_atstr_free(atclient_atstr *atstr)
+{
+    free(atstr->str);
+}
