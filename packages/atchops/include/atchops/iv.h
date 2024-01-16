42d49a588845308e6fdf0730e07e1f9101eb43d9
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-01-15 20:29:45 -0500

    docs: iv.h

--- packages/atchops/include/atchops/iv.h
+++ packages/atchops/include/atchops/iv.h
@@ -2,7 +2,22 @@
 #ifndef ATCHOPS_IV_H
 #define ATCHOPS_IV_H
 
+/**
+ * @brief Generates a random initialization vector (implied 16 bytes long)
+ * 
+ * @param iv the buffer to store the generated IV
+ * @return int 0 on success, non-zero on failure
+ */
 int atchops_iv_generate(unsigned char *iv);
+
+/**
+ * @brief GEnerates a random initialization vector and encodes it in base64
+ * 
+ * @param ivbase64 the buffer to store the generated IV
+ * @param ivbase64len the length of the buffer
+ * @param ivbase64olen the length of the generated IV
+ * @return int 0 on success, non-zero on failure
+ */
 int atchops_iv_generate_base64(unsigned char *ivbase64, const unsigned long ivbase64len, unsigned long *ivbase64olen);
 
 #endif
