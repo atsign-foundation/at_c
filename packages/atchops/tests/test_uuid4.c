31135e5b4eac8df0ffb2a00085c18170bcc6d784
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-01-18 17:28:53 -0500

    feat: uuid4 library and test_uuid4

---
+++ packages/atchops/tests/test_uuid4.c
@@ -0,0 +1,30 @@
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "atchops/uuid4.h"
+
+int main()
+{
+    int ret = 1;
+
+    const unsigned long buflen = 4096;
+    const unsigned char* buf = malloc(sizeof(unsigned char) * buflen);
+    memset(buf, 0, buflen);
+
+    uuid4_init();
+    uuid4_generate(buf);
+
+    printf("generated uuid4: %s\n", buf);
+
+    if(strlen(buf) > 0)
+    {
+        ret = 0;
+    }
+
+    goto exit;
+
+exit: {
+    return ret;
+}
+}
