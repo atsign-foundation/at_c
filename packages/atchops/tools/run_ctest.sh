9b70854c3e08941cf689f620d899484a473c5ae9
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-01-19 15:53:29 -0500

    chore: remove unused flags from scripts

--- packages/atchops/tools/run_ctest.sh
+++ packages/atchops/tools/run_ctest.sh
@@ -2,7 +2,7 @@
 set -eu
 cd ..
 sudo rm -rf build/tests
-cmake -S . -B build -DATCHOPS_BUILD_TESTS=ON -DATCHOPS_FETCH_MBEDTLS=ON
+cmake -S . -B build -DATCHOPS_BUILD_TESTS=ON
 cmake --build build --target all
 cd build/tests
 ctest -V
--- packages/atclient/tools/run_ctest.sh
+++ packages/atclient/tools/run_ctest.sh
@@ -2,7 +2,7 @@
 set -eu
 cd ..
 # sudo rm -rf build
-cmake -S . -B build -DATCLIENT_FETCH_ATCHOPS=ON -DATCLIENT_FETCH_MBEDTLS=ON -DATCLIENT_BUILD_TESTS=ON
+cmake -S . -B build -DATCLIENT_BUILD_TESTS=ON
 cmake --build build --target all
 cd build/tests
 ctest -V
