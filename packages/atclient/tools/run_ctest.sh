5393f39cd69a1f10804e0e28b62b84fc579da360
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-01-22 11:47:41 -0500

    feat: rm -rf build instead of build/tests in run_ctest.sh for more reliability

--- packages/atclient/tools/run_ctest.sh
+++ packages/atclient/tools/run_ctest.sh
@@ -4,7 +4,7 @@ FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 cd "$SCRIPT_DIRECTORY"
 cd ..
-rm -rf build/tests
+rm -rf build
 cmake -S . -B build -DATCLIENT_BUILD_TESTS=ON
 cmake --build build --target all
 cd build/tests
