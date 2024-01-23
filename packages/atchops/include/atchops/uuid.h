883bef530f675aefa552f7da9976e1aaab3f4aaf
Jeremy Tubongbanua <79019866+JeremyTubongbanua@users.noreply.github.com>
2024-01-23 10:09:15 -0500

    feat: uuid library in atchops using uuid4 fork (and new ctest tooling) (#86)
    * refactor: docs and organizing atclient CMake
    
    * feat: install scripts for atclient
    
    * feat: new dynamic find_package fetching, no longer needs to be specified
    
    * chore: remove unnecessary cmake configure flags in CI
    
    * docs: CMake
    
    * feat: uuid4 library and test_uuid4
    
    * refactor: 3rdparty/uuid
    
    * feat: remove uuid lib cause ESP32 unsupported platform
    
    * ci: remove fetch flags because no longer needed
    
    * chore: delete unneeded uuid4 test
    
    * chore: remove unused flags from scripts
    
    * ci: remove macos-latest because they both use the same compiler anyways
    
    * fix: add mbedcrypto
    
    * feat: remove macos
    
    * feat: use $SCRIPT_DIRECTORY in shell scripts
    
    * chore: remove sudo where it doesn't need to be used
    
    * chore: more logging
    
    * fix: use namespaces in atclient CMake
    
    * feat: rm -rf build instead of build/tests in run_ctest.sh for more reliability
    
    * feat: uuid.c, uuid.h, and test_uuid4.c in atchops
    
    * feat: use uuid4 release instead of git repository commit hash
    
    * feat: clean_run_ctest.sh and run_ctest.sh scripts
    
    * docs: atclient CMakeLists.txt
    
    * feat: fix export targets
    
    * fix: add uuid4-static to atclient export target

---
+++ packages/atchops/include/atchops/uuid.h
@@ -0,0 +1,7 @@
+#ifndef ATCHOPS_UUID_H
+#define ATCHOPS_UUID_H
+
+int atchops_uuid_init(void);
+int atchops_uuid_generate(char *uuidstr, int uuidstrlen);
+
+#endif
---
+++ packages/atchops/src/uuid.c
@@ -0,0 +1,31 @@
+
+#include "atchops/uuid.h"
+#include <string.h>
+#include <uuid4/uuid4.h>
+
+int atchops_uuid_init(void)
+{
+    return uuid4_init();
+}
+int atchops_uuid_generate(char *uuidstr, int uuidstrlen)
+{
+    int ret;
+    if (uuidstrlen < 37)
+    {
+        ret = 1; // UUID string is 36 characters long + 1 for null terminator = 37 minimum buffer length
+        goto exit;
+    }
+    uuid4_generate(uuidstr);
+    if(strlen(uuidstr) <= 0)
+    {
+        ret = 1; // an error occurred regarding the UUID generation and writing it to the buffer
+        goto exit;
+    }
+
+    ret = 0;
+    goto exit;
+exit:
+{
+    return ret;
+}
+}
