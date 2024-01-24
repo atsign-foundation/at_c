94b66df85c7ec7ba19313574323958add1faf595
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-01-24 10:55:18 -0500

    feat: cJSON test

--- packages/atclient/tests/test_cjson.c
+++ packages/atclient/tests/test_cjson.c
@@ -1,12 +1,34 @@
 
 
 #include <cJSON/cJSON.h>
+#include <stdio.h>
 
 int main()
 {
     int ret = 1; // error by default
 
+    /**
+     * Create this JSON and print
+     * {
+     *    "name": "bob",
+     *    "age": 5,
+     *    "stats": {
+     *        "defense": 4,
+     *        "hp": 10.0
+     *    }
+     * }
+     * 
+     */
+
     cJSON *root = cJSON_CreateObject();
+    cJSON_AddItemToObject(root, "name", cJSON_CreateString("bob"));
+    cJSON_AddItemToObject(root, "age", cJSON_CreateNumber(5));
+    cJSON *stats = cJSON_CreateObject();
+    cJSON_AddItemToObject(stats, "defense", cJSON_CreateNumber(4));
+    cJSON_AddItemToObject(stats, "hp", cJSON_CreateNumber(10.01));
+    cJSON_AddItemToObject(root, "stats", stats);
+    printf("%s\n", cJSON_Print(root));
+
     ret = 0;
 
     goto exit;
