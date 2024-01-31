a0c142c3bccbe28bc5a63fcf1e4fd58ce8b94576
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-01-31 16:11:37 -0500

    chore: move ATCHOPS_IV_SIZE to iv.h

--- packages/atchops/include/atchops/iv.h
+++ packages/atchops/include/atchops/iv.h
@@ -2,9 +2,11 @@
 #ifndef ATCHOPS_IV_H
 #define ATCHOPS_IV_H
 
+#define ATCHOPS_IV_SIZE 16 // ivs are always 16 bytes long
+
 /**
  * @brief Generates a random initialization vector (implied 16 bytes long)
- * 
+ *
  * @param iv the buffer to store the generated IV
  * @return int 0 on success, non-zero on failure
  */
@@ -12,7 +14,7 @@ int atchops_iv_generate(unsigned char *iv);
 
 /**
  * @brief Generates a random initialization vector and encodes it in base64
- * 
+ *
  * @param ivbase64 the buffer to store the generated IV
  * @param ivbase64len the length of the buffer
  * @param ivbase64olen the length of the generated IV
