54e15db2f337311cb79d490bd6bf47014238d26b
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-01-31 16:12:39 -0500

    feat: fix pkam_authenticate to use new atkeys implementation

--- examples/pkam_authenticate/develop.sh
+++ examples/pkam_authenticate/develop.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -eu
-sudo rm -rf /usr/local/include/atclient
-sudo rm -rf /usr/local/include/atchops
-cd ../../packages/atclient
-cmake -S . -B build -DATCLIENT_FETCH_MBEDTLS=OFF -DATCLIENT_FETCH_ATCHOPS=ON
-sudo cmake --build build --target install
-cd ../../examples/pkam_authenticate
+FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
+cd "$SCRIPT_DIRECTORY/../../packages/atclient"
 cmake -S . -B build
-cmake --build build --target clean
+sudo cmake --build build --target install
+cd "$SCRIPT_DIRECTORY/../../examples/pkam_authenticate"
+cmake -S . -B build
 cmake --build build --target all
 echo "Running main:"
 echo ""
