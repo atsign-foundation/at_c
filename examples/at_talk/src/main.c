21a6672d575279b63db25c0fadce195cb12850c3
realvarx <alvaromerarg@gmail.com>
2024-02-14 16:33:00 +0100

    fix: small format tweaks

--- examples/at_talk/src/main.c
+++ examples/at_talk/src/main.c
@@ -1,9 +1,9 @@
+#include <atclient/atclient.h>
+#include <atclient/atkeysfile.h>
+#include <atclient/atsign.h>
+#include <atlogger/atlogger.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <atclient/atclient.h>
-#include <atclient/atsign.h>
-#include <atclient/atkeysfile.h>
-#include <atlogger/atlogger.h>
 
 #define ROOT_HOST "root.atsign.org"
 #define ROOT_PORT 64
@@ -14,109 +14,98 @@
 
 #define TAG "at_talk"
 
-int main(int argc, char **argv)
-{
-    int ret = 0;
+int main(int argc, char **argv) {
+  int ret = 0;
 
-    atclient_atlogger_set_logging_level(ATLOGGER_LOGGING_LEVEL_INFO);
+  atclient_atlogger_set_logging_level(ATLOGGER_LOGGING_LEVEL_INFO);
 
-    // Init atclient
-    atclient atclient;
-    atclient_init(&atclient);
+  // Init atclient
+  atclient atclient;
+  atclient_init(&atclient);
 
-    // Init myatsign: the atsign that you'd like to use as a client
-    // and assign it to the atclient "atsign" parameter
-    atclient_atsign myatsign;
-    ret = atclient_atsign_init(&myatsign, ATSIGN);
-    if (ret != 0)
-    {
-        atclient_free(&atclient);
-        return ret;
-    }
-    atclient.atsign = myatsign;
-
-    // Init atkeysfile and read keys
-    atclient_atkeysfile atkeysfile;
-    atclient_atkeysfile_init(&atkeysfile);
-    ret = atclient_atkeysfile_read(&atkeysfile, ATKEYSFILE_PATH);
-    if (ret != 0)
-    {
-        atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "atclient_atkeys_file_read: %d\n", ret);
-        atclient_free(&atclient);
-        atclient_atkeysfile_free(&atkeysfile);
-        return ret;
-    }
-
-    // Init atkeys and assign them to the atclient "atkeys" parameter
-    atclient_atkeys atkeys;
-    atclient_atkeys_init(&atkeys);
-    ret = atclient_atkeys_populate_from_atkeysfile(&atkeys, atkeysfile);
-    if (ret != 0)
-    {
-        atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "atclient_atkeys_populate_from_atkeysfile: %d\n", ret);
-        goto exit1;
-    }
-    atclient.atkeys = atkeys;
-
-    // Root connection and pkam auth
-    ret = atclient_start_root_connection(&atclient, ROOT_HOST, ROOT_PORT);
-    if (ret != 0)
-    {
-        goto exit2;
-    }
-
-    ret = atclient_pkam_authenticate(&atclient, atclient.atkeys, ATSIGN, strlen(ATSIGN));
-    if (ret != 0)
-    {
-        goto exit2;
-    }
-    
-    // Init recipient's atsign
-    atclient_atsign recipient;
-    ret = atclient_atsign_init(&recipient, RECIPIENT);
-    if (ret != 0)
-    {
-        goto exit2;
-    }
-    
-    // Init variables and get the encryption keys
-    
-    char *enc_key_shared_by_me = malloc(45);
-    ret = atclient_get_encryption_key_shared_by_me(&atclient, &recipient, enc_key_shared_by_me);
-    if (ret != 0)
-    {
-        free(enc_key_shared_by_me);
-        goto exit2;
-    }
-
-    char *enc_key_shared_by_other = malloc(45);
-    ret = atclient_get_encryption_key_shared_by_other(&atclient, &recipient, enc_key_shared_by_other);
-    if (ret != 0)
-    {
-        free(enc_key_shared_by_me);
-        free(enc_key_shared_by_other);
-        goto exit2;
-    }
-    
-    atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "enc_key_shared_by_me: %s\n", enc_key_shared_by_me);
-    atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "enc_key_shared_by_other: %s\n", enc_key_shared_by_other);
-
+  // Init myatsign: the atsign that you'd like to use as a client
+  // and assign it to the atclient "atsign" parameter
+  atclient_atsign myatsign;
+  ret = atclient_atsign_init(&myatsign, ATSIGN);
+  if (ret != 0) {
+    atclient_free(&atclient);
     return ret;
+  }
+  atclient.atsign = myatsign;
 
-exit1:
-{
+  // Init atkeysfile and read keys
+  atclient_atkeysfile atkeysfile;
+  atclient_atkeysfile_init(&atkeysfile);
+  ret = atclient_atkeysfile_read(&atkeysfile, ATKEYSFILE_PATH);
+  if (ret != 0) {
+    atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "atclient_atkeys_file_read: %d\n", ret);
     atclient_free(&atclient);
     atclient_atkeysfile_free(&atkeysfile);
-    atclient_atkeys_free(&atkeys);
     return ret;
+  }
+
+  // Init atkeys and assign them to the atclient "atkeys" parameter
+  atclient_atkeys atkeys;
+  atclient_atkeys_init(&atkeys);
+  ret = atclient_atkeys_populate_from_atkeysfile(&atkeys, atkeysfile);
+  if (ret != 0) {
+    atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "atclient_atkeys_populate_from_atkeysfile: %d\n", ret);
+    goto exit1;
+  }
+  atclient.atkeys = atkeys;
+
+  // Root connection and pkam auth
+  ret = atclient_start_root_connection(&atclient, ROOT_HOST, ROOT_PORT);
+  if (ret != 0) {
+    goto exit2;
+  }
+
+  ret = atclient_pkam_authenticate(&atclient, atclient.atkeys, ATSIGN, strlen(ATSIGN));
+  if (ret != 0) {
+    goto exit2;
+  }
+
+  // Init recipient's atsign
+  atclient_atsign recipient;
+  ret = atclient_atsign_init(&recipient, RECIPIENT);
+  if (ret != 0) {
+    goto exit2;
+  }
+
+  // Init variables and get the encryption keys
+
+  char *enc_key_shared_by_me = malloc(45);
+  ret = atclient_get_encryption_key_shared_by_me(&atclient, &recipient, enc_key_shared_by_me);
+  if (ret != 0) {
+    free(enc_key_shared_by_me);
+    goto exit2;
+  }
+
+  char *enc_key_shared_by_other = malloc(45);
+  ret = atclient_get_encryption_key_shared_by_other(&atclient, &recipient, enc_key_shared_by_other);
+  if (ret != 0) {
+    free(enc_key_shared_by_me);
+    free(enc_key_shared_by_other);
+    goto exit2;
+  }
+
+  atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "enc_key_shared_by_me: %s\n", enc_key_shared_by_me);
+  atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "enc_key_shared_by_other: %s\n", enc_key_shared_by_other);
+
+  return ret;
+
+exit1 : {
+  atclient_free(&atclient);
+  atclient_atkeysfile_free(&atkeysfile);
+  atclient_atkeys_free(&atkeys);
+  return ret;
 }
-exit2:
-{
-    atclient_free(&atclient);
-    atclient_atkeysfile_free(&atkeysfile);
-    atclient_atkeys_free(&atkeys);
-    atclient_connection_disconnect(&(atclient.root_connection));
-    atclient_connection_free(&(atclient.root_connection));
-    return ret;
+exit2 : {
+  atclient_free(&atclient);
+  atclient_atkeysfile_free(&atkeysfile);
+  atclient_atkeys_free(&atkeys);
+  atclient_connection_disconnect(&(atclient.root_connection));
+  atclient_connection_free(&(atclient.root_connection));
+  return ret;
 }
 }
--- packages/atclient/src/atsign.c
+++ packages/atclient/src/atsign.c
@@ -1,74 +1,37 @@
-#include <string.h>
-#include <stdio.h>
-#include "atlogger/atlogger.h"
 #include "atclient/atsign.h"
 #include "atclient/atutils.h"
+#include "atlogger/atlogger.h"
+#include <stdio.h>
+#include <string.h>
 
 #define TAG "atsign"
 
-int atclient_atsign_init(atclient_atsign *atsign, char *atsign_str)
-{
-    int ret = 0;
+int atclient_atsign_init(atclient_atsign *atsign, char *atsign_str) {
+  int ret = 0;
 
-    // atsign_str is longer than expected or null/empty
-    if ((strlen(atsign_str) > 56) || (atsign_str == NULL) || (strlen(atsign_str) == 0)) {
-        ret = 1;
-        atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atclient_atsign_init: %d\n", ret);
-        return ret;
-    }
-
-    memset(atsign, 0, sizeof(atclient_atsign));
-    atsign->atsign = malloc(strlen(atsign_str) + 1);
-
-    const unsigned long maxatlen = 56;
-    unsigned long atolen = 0;
-    ret = atclient_atsign_with_at_symbol(atsign->atsign, maxatlen, &(atolen), atsign_str, strlen(atsign_str));
-    if(ret != 0)
-    {
-        atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atclient_atsign_with_at_symbol failed\n");
-        return ret;
-    }
-
-    atsign->without_prefix_str = atsign->atsign + 1;
+  // atsign_str is longer than expected or null/empty
+  if ((strlen(atsign_str) > 56) || (atsign_str == NULL) || (strlen(atsign_str) == 0)) {
+    ret = 1;
+    atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atclient_atsign_init: %d\n", ret);
     return ret;
-}
+  }
 
-void atclient_atsign_free(atclient_atsign* atsign) 
-{
-    free(atsign->atsign);
-}
+  memset(atsign, 0, sizeof(atclient_atsign));
+  atsign->atsign = malloc(strlen(atsign_str) + 1);
 
-int atclient_atsign_init(atclient_atsign *atsign, char *atsign_str)
-{
-    int ret = 0;
-
-    // atsign_str is longer than expected or null/empty
-    if ((strlen(atsign_str) > 56) || (atsign_str == NULL) || (strlen(atsign_str) == 0)) {
-        ret = 1;
-        atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atclient_atsign_init: %d\n", ret);
-        return ret;
-    }
-
-    memset(atsign, 0, sizeof(atclient_atsign));
-    atsign->atsign = malloc(strlen(atsign_str) + 1);
-
-    const unsigned long maxatlen = 56;
-    unsigned long atolen = 0;
-    ret = atclient_atsign_with_at_symbol(atsign->atsign, maxatlen, &(atolen), atsign_str, strlen(atsign_str));
-    if(ret != 0)
-    {
-        atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atclient_atsign_with_at_symbol failed\n");
-        return ret;
-    }
-
-    atsign->without_prefix_str = atsign->atsign + 1;
+  const unsigned long maxatlen = 56;
+  unsigned long atolen = 0;
+  ret = atclient_atsign_with_at_symbol(atsign->atsign, maxatlen, &(atolen), atsign_str, strlen(atsign_str));
+  if (ret != 0) {
+    atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atclient_atsign_with_at_symbol failed\n");
     return ret;
+  }
+
+  atsign->without_prefix_str = atsign->atsign + 1;
+  return ret;
 }
 
-void atclient_atsign_free(atclient_atsign* atsign) 
-{
-    free(atsign->atsign);
-}
+void atclient_atsign_free(atclient_atsign *atsign) { free(atsign->atsign); }
 
 int atclient_atsign_without_at_symbol(char *atsign, const unsigned long atsignlen, unsigned long *atsignolen,
                                       const char *originalatsign, const unsigned long originalatsignlen) {
@@ -93,15 +56,12 @@ int atclient_atsign_without_at_symbol(char *atsign, const unsigned long atsignle
     goto exit;
   }
 
-    strncpy(atsign, originalatsign + 1, originalatsignlen - 1);
-    atsign[originalatsignlen - 1] = '\0';
-    *atsignolen = originalatsignlen - 1;
-    ret = 0;
-    goto exit;
-exit:
-{
-    return ret;
-}
+  strncpy(atsign, originalatsign + 1, originalatsignlen - 1);
+  atsign[originalatsignlen - 1] = '\0';
+  *atsignolen = originalatsignlen - 1;
+  ret = 0;
+  goto exit;
+exit : { return ret; }
 }
 
 int atclient_atsign_with_at_symbol(char *atsign, const unsigned long atsignlen, unsigned long *atsignolen,
@@ -121,22 +81,19 @@ int atclient_atsign_with_at_symbol(char *atsign, const unsigned long atsignlen, 
     goto exit;
   }
 
-    if (originalatsign[0] == '@') {
-        // it already began with an x@x
-        strncpy(atsign, originalatsign, originalatsignlen + 1);
-        atsign[originalatsignlen] = '\0';
-        ret = 0;
-        goto exit;
-    }
-
-    atsign[0] = '@';
-    strncpy(atsign + 1, originalatsign, originalatsignlen + 1);
-    atsign[originalatsignlen + 1] = '\0';
-    *atsignolen = originalatsignlen + 1;
+  if (originalatsign[0] == '@') {
+    // it already began with an x@x
+    strncpy(atsign, originalatsign, originalatsignlen + 1);
+    atsign[originalatsignlen] = '\0';
     ret = 0;
     goto exit;
-exit:
-{
-    return ret;
-}
+  }
+
+  atsign[0] = '@';
+  strncpy(atsign + 1, originalatsign, originalatsignlen + 1);
+  atsign[originalatsignlen + 1] = '\0';
+  *atsignolen = originalatsignlen + 1;
+  ret = 0;
+  goto exit;
+exit : { return ret; }
 }
