f6b30b095f8f52e365d88ce3bde591d6c65ae543
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-02-14 13:52:36 -0500

    test: create public key

---
+++ packages/atclient/tests/test_atkey_create.c
@@ -0,0 +1,85 @@
+#include <string.h>
+#include "atclient/atkey.h"
+#include "atlogger/atlogger.h"
+
+#define TAG "test_atkey_create"
+
+static int test_create_publickey() {
+  int ret = 1;
+
+  atclient_atkey atkey;
+  atclient_atkey_init(&atkey);
+
+	char atkeystr[ATKEY_GENERAL_BUFFER_SIZE];
+	memset(atkeystr, 0, ATKEY_GENERAL_BUFFER_SIZE);
+
+	const char *expected = "public:test@alice";
+	const size_t expectedlen = strlen(expected);
+	size_t expectedolen = 0;
+
+  ret = atclient_atkey_create_publickey(&atkey, "test", strlen("test"), "@alice", strlen("@alice"), NULL, 0);
+  if (ret != 0) {
+    atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to create public key\n");
+    goto exit;
+  }
+
+	if(atkey.atkeytype != ATCLIENT_ATKEY_TYPE_PUBLICKEY) {
+		atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atkey type is not ATCLIENT_ATKEY_TYPE_PUBLICKEY, it is %d\n", atkey.atkeytype);
+		ret = 1;
+		goto exit;
+	}
+
+	if(strcmp(atkey.name.str, "test") != 0) {
+		atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atkey.name.str is not test, it is \"%s\"\n", atkey.name.str);
+		ret = 1;
+		goto exit;
+	}
+
+	if(strcmp(atkey.sharedby.str, "@alice") != 0) {
+		atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atkey.sharedby.str is not @alice, it is \"%s\"\n", atkey.sharedby.str);
+		ret = 1;
+		goto exit;
+	}
+
+	ret = atclient_atkey_to_string(atkey, atkeystr, ATKEY_GENERAL_BUFFER_SIZE, &expectedolen);
+	if(ret != 0) {
+		atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atclient_atkey_to_string: %d\n", ret);
+		ret = 1;
+		goto exit;
+	}
+
+	if(strcmp(atkeystr, expected) != 0) {
+		atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atkeystr is not %s, it is %s\n", expected, atkeystr);
+		ret = 1;
+		goto exit;
+	}
+
+	if(expectedolen != expectedlen) {
+		atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "expectedolen is not %lu, it is %lu\n", expectedlen, expectedolen);
+		ret = 1;
+		goto exit;
+	}
+
+  ret = 0;
+  goto exit;
+exit: {
+  atclient_atkey_free(&atkey);
+  return ret;
+}
+}
+
+int main() {
+  int ret = 1;
+
+	atclient_atlogger_set_logging_level(ATLOGGER_LOGGING_LEVEL_DEBUG);
+
+	ret = test_create_publickey();
+	if(ret != 0) {
+		atclient_atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "test_create_publickey: %d\n", ret);
+		goto exit;
+	}
+
+  ret = 0;
+  goto exit;
+exit: { return ret; }
+}
