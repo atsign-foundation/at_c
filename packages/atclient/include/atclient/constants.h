ad883a5625e5370607996b66b2652bb5c72560df
xavierchanth <xchanthavong@gmail.com>
2024-02-27 14:05:48 -0500

    chore: move things to related headers
    removed from constants.h and to their associated header file

--- packages/atclient/include/atclient/constants.h
+++ packages/atclient/include/atclient/constants.h
@@ -2,10 +2,6 @@
 #ifndef ATCLIENT_CONSTANTS_H
 #define ATCLIENT_CONSTANTS_H
 
-#define ATSIGN_BUFFER_LENGTH                                                                                           \
-  56 // sufficient memory for atSigns (current infrastructure restricts to a maximum of 55 utf7 chars)
-
 // TODO: remove this
-#define ATCLIENT_CONSTANTS_HOST_BUFFER_SIZE 128 // the size of the buffer for the host name
 
 #endif
