f0b889cac19ff8f24d233e642bc784b9596724ea
xavierchanth <xchanthavong@gmail.com>
2024-04-10 23:24:45 +0000

    fix: better dev script

--- packages/atclient/tools/dev.sh
+++ packages/atclient/tools/dev.sh
@@ -2,12 +2,18 @@
 
 script_dir="$(dirname -- "$(readlink -f -- "$0")")"
 package_dir="$script_dir/.."
-while true; do
-  # list for changes in the atclient directory
-  inotifywait -e modify,create,delete,move -r "$package_dir"
-  # rebuild the project
-  echo "Rebuilding atclient"
-  cmake -B "$package_dir/build" -S "$package_dir" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-  cmake --build "$package_dir/build" --target install
-  echo "Done rebuilding!"
+
+inotifywait -mrqe modify,move,delete "$package_dir" --format "%e %f" --exclude build | while read change; do
+  e=$(echo "$change" | cut -d' ' -f 1)
+  f=$(echo "$change" | cut -d' ' -f 2-)
+  if [[ "$f" = *.c ]] || [[ "$f" = *.h ]]; then
+    echo "Detected change: $e - $f"
+    echo "Compiling atclient"
+    cmake -B "$package_dir/build" -S "$package_dir" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON &>/dev/null
+    echo "Building atclient"
+    cmake --build "$package_dir/build" &>/dev/null
+    echo "Installing atclient"
+    sudo cmake --build "$package_dir/build" --target install &>/dev/null
+    echo "Done rebuilding!"
+  fi
 done
