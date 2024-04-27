bb4ebf18c7bce62c2875c719e164c515d40f1c00
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-04-27 10:55:49 -0400

    fix: sha test

--- packages/atchops/tests/test_sha.c
+++ packages/atchops/tests/test_sha.c
@@ -5,16 +5,19 @@
 #include <stddef.h>
 
 // dst: 33 4d 01 6f 75 5c d6 dc 58 c5 3a 86 e1 83 88 2f 8e c1 4f 52 fb 05 34 58 87 c8 a5 ed d4 2c 87 b7
-#define EXPETED_HASH {0x33, 0x4d, 0x01, 0x6f, 0x75, 0x5c, 0xd6, 0xdc, 0x58, 0xc5, 0x3a, 0x86, 0xe1, 0x83, 0x88, 0x2f, 0x8e, 0xc1, 0x4f, 0x52, 0xfb, 0x05, 0x34, 0x58, 0x87, 0xc8, 0xa5, 0xed, 0xd4, 0x2c, 0x87, 0xb7}
+#define EXPECTED_HASH {0x33, 0x4d, 0x01, 0x6f, 0x75, 0x5c, 0xd6, 0xdc, 0x58, 0xc5, 0x3a, 0x86, 0xe1, 0x83, 0x88, 0x2f, 0x8e, 0xc1, 0x4f, 0x52, 0xfb, 0x05, 0x34, 0x58, 0x87, 0xc8, 0xa5, 0xed, 0xd4, 0x2c, 0x87, 0xb7}
+
+#define HASH_SIZE 32
 
 int main() {
   int ret = 1;
 
   const char *src = "Hello!";
 
-  const size_t dstsize = 32;
-  unsigned char dst[dstsize];
-  memset(dst, 0, dstsize);
+  unsigned char dst[HASH_SIZE];
+  memset(dst, 0, sizeof(unsigned char) * HASH_SIZE);
+
+  const unsigned char expected_hash[HASH_SIZE] = EXPECTED_HASH;
 
   ret = atchops_sha_hash(MBEDTLS_MD_SHA256, (const unsigned char *)src, strlen(src), dst);
   if (ret != 0) {
@@ -25,13 +28,12 @@ int main() {
 
 
   printf("dst: ");
-  for (int i = 0; i < dstsize; i++) {
+  for (int i = 0; i < HASH_SIZE; i++) {
     printf("%02x ", dst[i]);
   }
   printf("\n");
 
-  unsigned char expected_hash[dstsize] = EXPETED_HASH;
-  ret = memcmp(dst, expected_hash, dstsize);
+  ret = memcmp(dst, expected_hash, HASH_SIZE);
   if (ret != 0) {
     printf("memcmp (failed): %d\n", ret);
     goto exit;
