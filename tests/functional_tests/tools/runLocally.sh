e14325ff05105ac7eff4b71af2f3ab7df944e460
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-04-28 19:10:02 -0400

    test: start on functional_tests

---
+++ tests/functional_tests/tools/runLocally.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
+cd "$SCRIPT_DIRECTORY"
+
+# 1. Install atSDK
+../../../tools/install.sh
+
+# 2. Run tests
+cmake -S .. -B ../build && cmake --build ../build && cd ../build && ctest --output-on-failure
