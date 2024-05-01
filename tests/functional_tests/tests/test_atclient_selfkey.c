7fa90ba7b74fffc54d842fdd3b1b41eb80bf0b42
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-05-01 16:49:28 -0400

    ci:

--- tests/functional_tests/tests/test_atclient_selfkey.c
+++ tests/functional_tests/tests/test_atclient_selfkey.c
@@ -1,6 +1,17 @@
-
+#include "functional_tests/config.h"
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
 
 int main()
 {
+    const size_t pathsize = 256;
+    char path [pathsize];
+    memset(path, 0, sizeof(char) * pathsize);
+    size_t pathlen;
+
+    get_atkeys_path(FIRST_ATSIGN, strlen(FIRST_ATSIGN), path, pathsize, &pathlen);
+
+    printf("Path: %s\n", path); // Path: keys/@12alpaca_key.atKeys
     return 0;
 }
