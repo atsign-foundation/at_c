42c6cfc3d6e510a5c225f2fe1f8ab29b4a6a047e
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-05-01 16:26:45 -0400

    ci: back to relative directory

--- tests/functional_tests/lib/src/config.c
+++ tests/functional_tests/lib/src/config.c
@@ -6,25 +6,25 @@
 #include "functional_tests/config.h"
 
 int get_atkeys_path(char *atsign, const size_t atsignlen, char *path, const size_t pathsize, size_t *pathlen) {
-  struct passwd *pw = getpwuid(getuid());
-  const char *homedir = pw->pw_dir;
-  const size_t kpathlen = strlen(homedir) + strlen("/.atsign/keys/") + atsignlen + strlen("_key.atkeys") + 1;
-  if (kpathlen > pathsize) {
-    return 1;
-  }
-  memset(path, 0, sizeof(char) * pathsize);
-  snprintf(path, pathsize, "%s/.atsign/keys/%s_key.atkeys", homedir, atsign);
-  *pathlen = kpathlen;
-
-  // get present working directory
-  // char cwd[256];
-  // memset(cwd, 0, sizeof(char) * 256);
-  // if (getcwd(cwd, 256) == NULL) {
+  // for home directory
+  // struct passwd *pw = getpwuid(getuid());
+  // const char *homedir = pw->pw_dir;
+  // const size_t kpathlen = strlen(homedir) + strlen("/.atsign/keys/") + atsignlen + strlen("_key.atkeys") + 1;
+  // if (kpathlen > pathsize) {
   //   return 1;
   // }
-  // snprintf(path, pathsize, "keys/%.*s_key.atKeys", (int) atsignlen, atsign);
-  // snprintf(path, pathsize, "%s/keys/%.*s_key.atKeys", cwd, (int) atsignlen, atsign);
-  // *pathlen = strlen(path);
+  // memset(path, 0, sizeof(char) * pathsize);
+  // snprintf(path, pathsize, "%s/.atsign/keys/%s_key.atkeys", homedir, atsign);
+  // *pathlen = kpathlen;
+
+  // get present working directory
+  char cwd[256];
+  memset(cwd, 0, sizeof(char) * 256);
+  if (getcwd(cwd, 256) == NULL) {
+    return 1;
+  }
+  snprintf(path, pathsize, "keys/%.*s_key.atKeys", (int) atsignlen, atsign); // keys/@bob_key.atKeys
+  *pathlen = strlen(path);
 
   return 0;
 }
