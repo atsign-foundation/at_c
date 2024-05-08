091e4634367c5fe9e6f81f021617b328631cd105
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-05-08 15:47:57 -0400

    test: remove heart beat from functional test

--- tests/functional_tests/tests/test_atclient_monitor.c
+++ tests/functional_tests/tests/test_atclient_monitor.c
@@ -10,7 +10,6 @@
 #include <atclient/notify.h>
 #include <atclient/stringutils.h>
 #include <atlogger/atlogger.h>
-#include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,10 +28,9 @@
 
 static int monitor_pkam_auth(atclient *monitor_conn, const atclient_atkeys *atkeys, const char *atsign,
                              const size_t atsignlen);
-static void *heartbeat_handler(void *heartbeat_conn);
 static int test_1_start_monitor(atclient *monitor_conn);
 static int test_2_send_notification(atclient *atclient);
-static int test_3_read_notification(atclient *monitor_conn);
+static int test_3_monitor_for_notification(atclient *monitor_conn);
 
 int main() {
   int ret = 1;
@@ -50,11 +48,6 @@ int main() {
 
   atclient_atkeys atkeys_sharedwith;
   atclient_atkeys_init(&atkeys_sharedwith);
-
-  atclient heartbeat_conn;
-  atclient_init(&heartbeat_conn);
-
-  pthread_t tid;
 
   if ((ret = functional_tests_set_up_atkeys(&atkeys_sharedby, ATKEY_SHAREDBY, strlen(ATKEY_SHAREDBY))) != 0) {
     atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to set up atkeys_sharedby: %d\n", ret);
@@ -76,11 +69,6 @@ int main() {
     goto exit;
   }
 
-  if((ret = monitor_pkam_auth(&heartbeat_conn, &atkeys_sharedwith, ATKEY_SHAREDWITH, strlen(ATKEY_SHAREDWITH))) != 0) {
-    atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to authenticate with PKAM: %d\n", ret);
-    goto exit;
-  }
-
   if ((ret = test_1_start_monitor(&monitor_conn)) != 0) {
     atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "test_1_start_monitor: %d\n", ret);
     goto exit;
@@ -91,14 +79,14 @@ int main() {
     goto exit;
   }
 
-  if ((ret = test_3_read_notification(&monitor_conn)) != 0) {
+  if ((ret = test_3_monitor_for_notification(&monitor_conn)) != 0) {
     atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "test_4_read_notification: %d\n", ret);
     goto exit;
   }
 
   goto exit;
 exit: {
-  if((ret = functional_tests_tear_down_sharedenckeys(&atclient1, ATKEY_SHAREDWITH)) != 0) {
+  if ((ret = functional_tests_tear_down_sharedenckeys(&atclient1, ATKEY_SHAREDWITH)) != 0) {
     ret = 1;
     atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to tear down sharedenckeys: %d\n", ret);
   }
@@ -106,7 +94,6 @@ exit: {
   atclient_atkeys_free(&atkeys_sharedwith);
   atclient_free(&atclient1);
   atclient_free(&monitor_conn);
-  atclient_free(&heartbeat_conn);
   return ret;
 }
 }
@@ -156,25 +143,6 @@ exit: {
 }
 }
 
-static int test_2_start_heartbeat(atclient *heartbeat_conn, pthread_t *tid) {
-  int ret = 1;
-
-  atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "test_2_start_heartbeat Start\n");
-
-  ret = pthread_create(tid, NULL, heartbeat_handler, heartbeat_conn);
-  if (ret != 0) {
-    atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to start heartbeat handler: %d\n", ret);
-    goto exit;
-  }
-
-  ret = 0;
-  goto exit;
-exit: {
-  atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "test_2_start_heartbeat End: %d\n", ret);
-  return ret;
-}
-}
-
 static int test_2_send_notification(atclient *atclient) {
   int ret = 1;
 
@@ -209,56 +177,58 @@ exit: {
 }
 }
 
-static int test_3_read_notification(atclient *monitor_conn) {
+static int test_3_monitor_for_notification(atclient *monitor_conn) {
   int ret = 1;
 
   atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "test_4_read_notification Start\n");
 
   atclient_monitor_message *message = NULL;
 
-  int tries = 5;
-  int i = 0;
+  int max_tries = 5;
+  int tries = 1;
 
-  while (i < tries) {
+  while (tries < max_tries) {
     if ((ret = atclient_monitor_read(monitor_conn, &message)) != 0) {
       atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to read monitor message: %d\n", ret);
       goto exit;
     }
 
     if (message == NULL) {
-      atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "monitor message is NULL, when it is expected to be populated :(\n");
-      i++;
+      atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO,
+                   "monitor message is NULL, when it is expected to be populated :(\n");
+      tries++;
       continue;
     }
 
-    if(!atclient_atnotification_decryptedvalue_is_initialized(&(message->notification))) {
+    if (!atclient_atnotification_decryptedvalue_is_initialized(&(message->notification))) {
       atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "Decrypted value is not initialized\n");
-      i++;
+      tries++;
       continue;
     }
 
-    if(!atclient_atnotification_decryptedvaluelen_is_initialized(&(message->notification))) {
+    if (!atclient_atnotification_decryptedvaluelen_is_initialized(&(message->notification))) {
       atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "Decrypted value length is not initialized\n");
-      i++;
+      tries++;
       continue;
     }
 
-    atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "Decrypted Value (%lu): %s\n", (int) message->notification.decryptedvaluelen, message->notification.decryptedvalue);
+    atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "Decrypted Value (%lu): %s\n",
+                 (int)message->notification.decryptedvaluelen, message->notification.decryptedvalue);
 
     // compare the decrypted value with the expected value
     if (strcmp(message->notification.decryptedvalue, ATKEY_VALUE) != 0) {
       atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "Decrypted value does not match expected value\n");
-      i++;
+      tries++;
       continue;
     }
 
-    atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "Tries: %d\n", i);
+    atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "Tries: %d\n", tries);
 
     ret = 0;
     goto exit;
   }
 
-  atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to read monitor message after %d tries\n", tries);
+  atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to read monitor message after %d tries\n", max_tries);
 
   ret = 1;
   goto exit;
