08868221a1bde10b3b5450d9c85acec688ee4c1e
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-05-15 11:09:20 -0400

    feat: base64 optimal length helper function

--- packages/atchops/include/atchops/base64.h
+++ packages/atchops/include/atchops/base64.h
@@ -29,4 +29,21 @@ int atchops_base64_encode(const unsigned char *src, const size_t srcsize, unsign
 int atchops_base64_decode(const unsigned char *src, const size_t srcsize, unsigned char *dst,
                           size_t dstsize, size_t *dstlen);
 
+
+/**
+ * @brief calculate the size of the base64 encoded string. This function is usually called before encoding to allocate the optimal buffer size
+ * 
+ * @param plaintextsize the size of the original plain text (to be encoded)
+ * @return size_t the output buffer size needed to store the encoded base64 string
+ */
+size_t atchops_base64_encoded_size(const size_t plaintextsize);
+
+/**
+ * @brief calculate the size of the base64 decoded string. This function is usually called before decoding to allocate the optimal buffer size
+ * 
+ * @param encodedsize the size of the base64 encoded string (to be decoded)
+ * @return size_t the output buffer size needed to store the decoded plain text
+ */
+size_t atchops_base64_decoded_size(const size_t encodedsize);
+
 #endif
