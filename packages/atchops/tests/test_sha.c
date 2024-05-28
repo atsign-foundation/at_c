716c2a595c0d8348c34b986069861f7920bfa35a
xavierchanth <xchanthavong@gmail.com>
2024-05-28 11:48:27 -0400

    fix: sha test

--- packages/atchops/tests/test_sha.c
+++ packages/atchops/tests/test_sha.c
@@ -1,11 +1,14 @@
 #include "atchops/sha.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
 // dst: 33 4d 01 6f 75 5c d6 dc 58 c5 3a 86 e1 83 88 2f 8e c1 4f 52 fb 05 34 58 87 c8 a5 ed d4 2c 87 b7
-#define EXPECTED_HASH {0x33, 0x4d, 0x01, 0x6f, 0x75, 0x5c, 0xd6, 0xdc, 0x58, 0xc5, 0x3a, 0x86, 0xe1, 0x83, 0x88, 0x2f, 0x8e, 0xc1, 0x4f, 0x52, 0xfb, 0x05, 0x34, 0x58, 0x87, 0xc8, 0xa5, 0xed, 0xd4, 0x2c, 0x87, 0xb7}
+#define EXPECTED_HASH                                                                                                  \
+  {                                                                                                                    \
+    0x33, 0x4d, 0x01, 0x6f, 0x75, 0x5c, 0xd6, 0xdc, 0x58, 0xc5, 0x3a, 0x86, 0xe1, 0x83, 0x88, 0x2f, 0x8e, 0xc1, 0x4f,  \
+        0x52, 0xfb, 0x05, 0x34, 0x58, 0x87, 0xc8, 0xa5, 0xed, 0xd4, 0x2c, 0x87, 0xb7                                   \
+  }
 
 #define HASH_SIZE 32
 
@@ -19,13 +22,12 @@ int main() {
 
   const unsigned char expected_hash[HASH_SIZE] = EXPECTED_HASH;
 
-  ret = atchops_sha_hash(MBEDTLS_MD_SHA256, (const unsigned char *)src, strlen(src), dst);
+  ret = atchops_sha_hash(ATCHOPS_MD_SHA256, (const unsigned char *)src, strlen(src), dst);
   if (ret != 0) {
     printf("atchops_sha_hash (failed): %d\n", ret);
     goto exit;
   }
   printf("atchops_sha_hash (success): %d\n", ret);
-
 
   printf("dst: ");
   for (int i = 0; i < HASH_SIZE; i++) {
@@ -44,7 +46,5 @@ int main() {
 
   goto exit;
 
-exit: {
-  return ret;
-}
+exit: { return ret; }
 }
