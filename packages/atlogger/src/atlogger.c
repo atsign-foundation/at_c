d68cb1584a64735e9a4ba7ad5d146d7656cc3664
xavierchanth <xchanthavong@gmail.com>
2024-06-03 12:13:58 -0400

    chore: format iso 8601 string with |

--- packages/atlogger/src/atlogger.c
+++ packages/atlogger/src/atlogger.c
@@ -69,7 +69,7 @@ static void atlogger_get_prefix(enum atlogger_logging_level logging_level, char 
     int res = clock_gettime(CLOCK_REALTIME, &timespec);
 
     if (res == 0) {
-      res = strftime(prefix + off, PREFIX_BUFFER_LEN - off, "%F %T",
+      res = strftime(prefix + off, PREFIX_BUFFER_LEN - off, " | %F %T",
                      gmtime(&timespec.tv_sec)); // format accurate to the second
       if (res != 0) {
         off += res;
@@ -79,9 +79,10 @@ static void atlogger_get_prefix(enum atlogger_logging_level logging_level, char 
 
     if (res == 0) {
       snprintf(prefix + off, PREFIX_BUFFER_LEN - off, ".%ld", timespec.tv_nsec);
-      // TODO: future modifications to the prefix require calculation of the new offset
-      // off += ?
+      off += strlen(prefix + off);
     }
+
+    snprintf(prefix + off, PREFIX_BUFFER_LEN - off, " | ");
   }
 }
 
