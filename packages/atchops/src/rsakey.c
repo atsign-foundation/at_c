e66d7380cca5f4b036e02ac353c041926725eb3c
Xavier Chanthavong <xchanthavong@gmail.com>
2024-06-14 11:03:23 -0400

    feat: rsakey deep clone (#321)

--- packages/atchops/src/rsakey.c
+++ packages/atchops/src/rsakey.c
@@ -1,9 +1,9 @@
 #include "atchops/rsakey.h"
 #include "atchops/base64.h"
 #include <mbedtls/asn1.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
 
 #define BASE64_DECODED_KEY_BUFFER_SIZE 8192 // the max buffer size of a decoded RSA key
 
@@ -16,6 +16,19 @@ void atchops_rsakey_publickey_init(atchops_rsakey_publickey *publickey) {
   publickey->e.len = BASE64_DECODED_KEY_BUFFER_SIZE;
   publickey->e.value = (unsigned char *)malloc(sizeof(unsigned char) * publickey->e.len);
 }
+
+void atchops_rsakey_publickey_clone(atchops_rsakey_publickey *dst, atchops_rsakey_publickey *src) {
+  memset(dst, 0, sizeof(atchops_rsakey_publickey));
+
+  dst->n.len = src->n.len;
+  dst->n.value = (unsigned char *)malloc(sizeof(unsigned char) * dst->n.len);
+  memcpy(dst->n.value, src->n.value, dst->n.len);
+
+  dst->e.len = src->e.len;
+  dst->e.value = (unsigned char *)malloc(sizeof(unsigned char) * dst->e.len);
+  memcpy(dst->e.value, src->e.value, dst->e.len);
+}
+
 void atchops_rsakey_publickey_free(atchops_rsakey_publickey *publickey) {
   free(publickey->n.value);
   free(publickey->e.value);
@@ -38,6 +51,29 @@ void atchops_rsakey_privatekey_init(atchops_rsakey_privatekey *privatekey) {
 
   privatekey->q.len = BASE64_DECODED_KEY_BUFFER_SIZE;
   privatekey->q.value = malloc(sizeof(unsigned char) * privatekey->q.len);
+}
+
+void atchops_rsakey_privatekey_clone(atchops_rsakey_privatekey *dst, atchops_rsakey_privatekey *src) {
+  memset(dst, 0, sizeof(atchops_rsakey_privatekey));
+  dst->n.len = src->n.len;
+  dst->n.value = (unsigned char *)malloc(sizeof(unsigned char) * dst->n.len);
+  memcpy(dst->n.value, src->n.value, dst->n.len);
+
+  dst->e.len = src->e.len;
+  dst->e.value = (unsigned char *)malloc(sizeof(unsigned char) * dst->e.len);
+  memcpy(dst->e.value, src->e.value, dst->e.len);
+
+  dst->d.len = src->d.len;
+  dst->d.value = (unsigned char *)malloc(sizeof(unsigned char) * dst->d.len);
+  memcpy(dst->d.value, src->d.value, dst->d.len);
+
+  dst->p.len = src->p.len;
+  dst->p.value = (unsigned char *)malloc(sizeof(unsigned char) * dst->p.len);
+  memcpy(dst->p.value, src->p.value, dst->p.len);
+
+  dst->q.len = src->q.len;
+  dst->q.value = (unsigned char *)malloc(sizeof(unsigned char) * dst->q.len);
+  memcpy(dst->q.value, src->q.value, dst->q.len);
 }
 
 void atchops_rsakey_privatekey_free(atchops_rsakey_privatekey *privatekey) {
@@ -90,7 +126,7 @@ int atchops_rsakey_populate_publickey(atchops_rsakey_publickey *publickey, const
   }
 
   seq = malloc(sizeof(mbedtls_asn1_sequence));
-  memset(seq, 0, sizeof(mbedtls_asn1_sequence));  
+  memset(seq, 0, sizeof(mbedtls_asn1_sequence));
   ret = mbedtls_asn1_get_sequence_of(&p, end, seq, MBEDTLS_ASN1_INTEGER);
   if (ret != 0) {
     goto exit;
@@ -102,7 +138,7 @@ int atchops_rsakey_populate_publickey(atchops_rsakey_publickey *publickey, const
 
   current = current->next;
   publickey->e.len = current->buf.len;
-  memcpy(publickey->e.value, current->buf.p, publickey->e.len);  
+  memcpy(publickey->e.value, current->buf.p, publickey->e.len);
 
   ret = 0;
   goto exit;
