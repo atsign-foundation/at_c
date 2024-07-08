402b8d108fd522d0bf8923f11a432eab591bd272
Jeremy Tubongbanua <79019866+JeremyTubongbanua@users.noreply.github.com>
2024-07-08 10:38:04 -0400

    feat: atclient_get_atkeys (#330)
    * feat: atclient_get_atkeys.c boilerplate
    
    * chore: alphabetical order ATCLIENT_SOURCES
    
    * test: boiler plate test_atclient_get_atkeys functional test
    
    * feat: functional atclient_get_atkeys

--- packages/atclient/include/atclient/atclient.h
+++ packages/atclient/include/atclient/atclient.h
@@ -20,7 +20,7 @@ typedef struct atclient {
   bool _atsign_is_allocated : 1; // internally used for memory management
 
   atclient_atkeys atkeys;
-  
+
   // Warning! async_read is an experimental feature and not fully implemented.
   // You should leave this set to false unless you know what you are doing.
   bool async_read;
@@ -167,6 +167,21 @@ int atclient_get_sharedkey(atclient *atclient, atclient_atkey *atkey, char *valu
  * @return int 0 on success
  */
 int atclient_delete(atclient *atclient, const atclient_atkey *atkey);
+
+/**
+ * @brief Runs a scan of the atServer to see what atKeys you have.
+ *
+ * @param atclient the initialized and pkam_authenticated atclient context
+ * @param regex (input): the regex pattern for what keys to filter
+ * @param showhidden (input): true to show "hidden" keys (which are keys that begin with an _), false otherwise
+ * @param recvbuffersize (input): the size of the buffer to receive the response from the atServer
+ * @param atkey (output): a double pointer which will be a pointer to the first atkey in the array. caller of this
+ * function is responsible for each individual atkey in the array using `atclient_atkey_free`
+ * @param output_array_len (output): the overall size of the array that was allocated
+ * @return int 0 on success
+ */
+int atclient_get_atkeys(atclient *atclient, const char *regex, const bool showhidden, const size_t recvbuffersize,
+                        atclient_atkey **atkey, size_t *output_array_len);
 
 /**
  * @brief Send a heartbeat (noop)
