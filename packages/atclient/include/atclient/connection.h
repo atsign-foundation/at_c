d082fc2e67fe17531d2b2fa71dad6168e5a4ea85
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-07-29 10:45:15 -0400

    feat: connection_hooks.h/.c

--- packages/atclient/include/atclient/connection.h
+++ packages/atclient/include/atclient/connection.h
@@ -7,8 +7,7 @@
 #include <mbedtls/ssl.h>
 #include <stdbool.h>
 #include <stddef.h>
-
-#define ATCLIENT_CONSTANTS_HOST_BUFFER_SIZE 128 // the size of the buffer for the host name
+#include "atclient/connection_hooks.h"
 
 // represents the type of connection
 typedef enum atclient_connection_type {
@@ -16,39 +15,21 @@ typedef enum atclient_connection_type {
   ATCLIENT_CONNECTION_TYPE_ATSERVER     // uses 'noop:0\r\n' to check if it is connected
 } atclient_connection_type;
 
-typedef int(atclient_connection_send_hook)(const unsigned char *src, const size_t src_len, unsigned char *recv,
-                                           const size_t recv_size, size_t *recv_len);
-
-typedef enum atclient_connection_hook_type {
-  ATCLIENT_CONNECTION_HOOK_TYPE_NONE = 0,
-  ATCLIENT_CONNECTION_HOOK_TYPE_PRE_SEND,
-  ATCLIENT_CONNECTION_HOOK_TYPE_POST_SEND,
-  ATCLIENT_CONNECTION_HOOK_TYPE_PRE_RECV,
-  ATCLIENT_CONNECTION_HOOK_TYPE_POST_RECV,
-} atclient_connection_hook_type;
-
-typedef struct atclient_connection_hooks {
-  bool _is_nested_call; // internal variable for preventing infinite recursion (hooks cannot trigger other hooks in
-                        // their nested calls)
-  atclient_connection_send_hook *pre_send;
-  atclient_connection_send_hook *post_send;
-  atclient_connection_send_hook *pre_recv;
-  atclient_connection_send_hook *post_recv;
-  bool readonly_src;
-} atclient_connection_hooks;
-
 typedef struct atclient_connection {
-  atclient_connection_type type;
+  atclient_connection_type type; // set in atclient_connection_init
 
-  char host[ATCLIENT_CONSTANTS_HOST_BUFFER_SIZE];
-  int port; // example: 64
+  bool _is_host_initialized: 1;
+  char *host; // example: "root.atsign.org"
+
+  bool _is_port_initialized: 1;
+  uint16_t port; // example: 64
 
   // atclient_connection_connect sets this to true and atclient_connection_disconnect sets this to false
   // this does not mean that the connection is still alive, it just means that the connection was established at least
   // once, at some  point, check atclient_connection_is_connected for a live status on the connection
-  // _should_be_connected also serves as an internal boolean to check if the following mbedlts contexts have been
+  // _is_connection_enabled also serves as an internal boolean to check if the following mbedlts contexts have been
   // initialized and need to be freed at the end
-  bool _should_be_connected;
+  bool _is_connection_enabled: 1;
   mbedtls_net_context net;
   mbedtls_ssl_context ssl;
   mbedtls_ssl_config ssl_config;
@@ -56,7 +37,7 @@ typedef struct atclient_connection {
   mbedtls_entropy_context entropy;
   mbedtls_ctr_drbg_context ctr_drbg;
 
-  bool _is_hooks_enabled;
+  bool _is_hooks_enabled: 1;
   atclient_connection_hooks *hooks;
 } atclient_connection;
 
@@ -71,6 +52,13 @@ typedef struct atclient_connection {
 void atclient_connection_init(atclient_connection *ctx, atclient_connection_type type);
 
 /**
+ * @brief free memory allocated by the init function
+ *
+ * @param ctx the struct which was previously initialized
+ */
+void atclient_connection_free(atclient_connection *ctx);
+
+/**
  * @brief after initializing a connection context, connect to a host and port
  *
  * @param ctx the initialized context
@@ -78,7 +66,28 @@ void atclient_connection_init(atclient_connection *ctx, atclient_connection_type
  * @param port the port to connect to
  * @return int 0 on success, otherwise error
  */
-int atclient_connection_connect(atclient_connection *ctx, const char *host, const int port);
+int atclient_connection_connect(atclient_connection *ctx, const char *host, const uint16_t port);
+
+/**
+ * @brief Reads data from the connection
+ *
+ * @param ctx the connection initialized and connected using atclient_connection_init and atclient_connection_connect
+ * @param value a double pointer that will be allocated by the function to the data read
+ * @param value_len the length of the data read, will be set by the function
+ * @param value_max_len the maximum length of the data to read, setting this to 0 means no limit
+ * @return int 0 on success
+ */
+int atclient_connection_read(atclient_connection *ctx, unsigned char **value, size_t *value_len, const size_t value_max_len);
+
+/**
+ * @brief Write data to the connection
+ * 
+ * @param ctx connection initialized and connected using atclient_connection_init and atclient_connection_connect
+ * @param value the data to write
+ * @param value_len the length of the data to write
+ * @return int 0 on success
+ */
+int atclient_connection_write(atclient_connection *ctx, const unsigned char *value, const size_t value_len);
 
 /**
  * @brief send data to the connection
@@ -111,61 +120,5 @@ int atclient_connection_disconnect(atclient_connection *ctx);
  * @return true if the connection is connected, otherwise false if it is not connected or an error occurred
  */
 bool atclient_connection_is_connected(atclient_connection *ctx);
-
-/**
- * @brief free memory allocated by the init function
- *
- * @param ctx the struct which was previously initialized
- */
-void atclient_connection_free(atclient_connection *ctx);
-
-/**
- * @brief Initialize the hooks memory allocation
- *
- * @param ctx the struct for the connection
- */
-void atclient_connection_enable_hooks(atclient_connection *ctx);
-
-/**
- * @brief Add a hook to be called during the connection lifecycle
- *
- * @param ctx the struct for the connection
- * @param type the hook type you want to add
- * @param hook the hook function itself
- *
- * @return int 0 on success, otherwise error
- */
-int atclient_connection_hooks_set(atclient_connection *ctx, atclient_connection_hook_type type, void *hook);
-
-/**
- * @brief Set whether the readonly_src status for all hooks
- *
- * @param ctx the struct for the connection
- * @param readonly_src the new state for readonly_src
- *
- * @note For performance, keep readonly_src set to true if you don't need to write access to src
- */
-void atclient_connection_hooks_set_readonly_src(atclient_connection *ctx, bool readonly_src);
-
-/**
- * @brief Write data to the connection
- * 
- * @param ctx connection initialized and connected using atclient_connection_init and atclient_connection_connect
- * @param value the data to write
- * @param value_len the length of the data to write
- * @return int 0 on success
- */
-int atclient_connection_write(atclient_connection *ctx, const unsigned char *value, const size_t value_len);
-
-/**
- * @brief Reads data from the connection
- *
- * @param ctx the connection initialized and connected using atclient_connection_init and atclient_connection_connect
- * @param value a double pointer that will be allocated by the function to the data read
- * @param value_len the length of the data read, will be set by the function
- * @param value_max_len the maximum length of the data to read, setting this to 0 means no limit
- * @return int 0 on success
- */
-int atclient_connection_read(atclient_connection *ctx, unsigned char **value, size_t *value_len, const size_t value_max_len);
 
 #endif
