f761cfd2b34dec1a3f79b793b4f43341a6d5438a
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-08-06 12:36:06 -0400

    chore: repl example fixes

--- examples/desktop/repl/run.sh
+++ examples/desktop/repl/run.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 set -eu
 
-# clean
-rm -f build/CMakeCache.txt
-sudo rm -f bin/repl
-
-# install dependencies
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
-
-# 1. Install atsdk
-"$SCRIPT_DIRECTORY/../../../tools/install.sh"
-
-# 2. Build REPL
 cd "$SCRIPT_DIRECTORY"
 
+# clean
+rm -f build/CMakeCache.txt
+rm -f bin/repl
+
+# 1. Install atsdk
+"$SCRIPT_DIRECTORY/../../../tools/debug_install.sh"
+
+# 2. Build REPL
+
 ## 2a. CMake configure
-cmake -S . -B build
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc
 
 ## 2b. CMake build
 cmake --build build --target install
