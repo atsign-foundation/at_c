a5819c518c566bf569c4bb2fd56def58f394f412
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-09-26 16:43:49 +0000

    test: improve test_rsa_generate

--- packages/atchops/tests/test_rsa_generate.c
+++ packages/atchops/tests/test_rsa_generate.c
@@ -1,6 +1,9 @@
 
 #include <atchops/rsa_key.h>
+#include <atchops/rsa.h>
 #include <atlogger/atlogger.h>
+
+#define PLAINTEXT "Hello, World!"
 
 int main() {
 
@@ -13,6 +16,14 @@ int main() {
 
   atchops_rsa_key_private_key private_key;
   atchops_rsa_key_private_key_init(&private_key);
+
+  const size_t ciphertext_size = 256;
+  unsigned char ciphertext[ciphertext_size];
+  memset(ciphertext, 0, sizeof(unsigned char) * ciphertext_size);
+
+  const size_t plaintext_size = 256;
+  unsigned char plaintext[plaintext_size];
+  memset(plaintext, 0, sizeof(unsigned char) * plaintext_size);
 
   if ((ret = atchops_rsa_key_generate(&public_key, &private_key)) != 0) {
     atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to generate RSA key pair\n");
@@ -69,6 +80,40 @@ int main() {
     atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_ERROR, "Private key is not populated\n");
     goto exit;
   }
+
+  // use the public key to encrypt something
+  // use the private key to decrypt it
+
+  if((ret = atchops_rsa_encrypt(&public_key, (const unsigned char *)PLAINTEXT, strlen(PLAINTEXT), ciphertext)) != 0) {
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to encrypt plaintext\n");
+    goto exit;
+  }
+
+  // log the ciphertext
+  atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_INFO, "Ciphertext: ");
+  for (size_t i = 0; i < ciphertext_size; i++) {
+    printf("%02x ", ciphertext[i]);
+  }
+  
+  size_t plaintext_len = 0;
+  if((ret = atchops_rsa_decrypt(&private_key, ciphertext, ciphertext_size, plaintext, plaintext_size, &plaintext_len)) != 0) {
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to decrypt ciphertext\n");
+    goto exit;
+  }
+
+  // log the plaintext
+  atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_INFO, "Plaintext: ");
+  for (size_t i = 0; i < plaintext_len; i++) {
+    printf("%c", plaintext[i]);
+  }
+  printf("\n");
+
+  // check if plaintext is equal to PLAINTEXT
+  if (strncmp((const char *)plaintext, PLAINTEXT, strlen(PLAINTEXT)) != 0) {
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_ERROR, "Plaintext does not match original\n");
+    goto exit;
+  }
+
 
 exit: {
   atchops_rsa_key_public_key_free(&public_key);
