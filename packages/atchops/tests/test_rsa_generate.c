912b3fe94bf0965a8bbeebee504dd5465e1c99ad
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-09-26 16:32:33 +0000

    chore: logging in test

--- packages/atchops/tests/test_rsa_generate.c
+++ packages/atchops/tests/test_rsa_generate.c
@@ -19,6 +19,57 @@ int main() {
     goto exit;
   }
 
+  // log the public key
+  if (atchops_rsa_key_is_public_key_populated(&public_key)) {
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_INFO, "Public Key:\n");
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_INFO, "N: ");
+    for (size_t i = 0; i < public_key.n.len; i++) {
+      printf("%02x ", public_key.n.value[i]);
+    }
+    printf("\n");
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_INFO, "E: ");
+    for (size_t i = 0; i < public_key.e.len; i++) {
+      printf("%02x ", public_key.e.value[i]);
+    }
+    printf("\n");
+  } else {
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_ERROR, "Public key is not populated\n");
+    goto exit;
+  }
+
+  // log the private key
+  if (atchops_rsa_key_is_private_key_populated(&private_key)) {
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_INFO, "Private Key:\n");
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_INFO, "N: ");
+    for (size_t i = 0; i < private_key.n.len; i++) {
+      printf("%02x ", private_key.n.value[i]);
+    }
+    printf("\n");
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_INFO, "E: ");
+    for (size_t i = 0; i < private_key.e.len; i++) {
+      printf("%02x ", private_key.e.value[i]);
+    }
+    printf("\n");
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_INFO, "D: ");
+    for (size_t i = 0; i < private_key.d.len; i++) {
+      printf("%02x ", private_key.d.value[i]);
+    }
+    printf("\n");
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_INFO, "P: ");
+    for (size_t i = 0; i < private_key.p.len; i++) {
+      printf("%02x ", private_key.p.value[i]);
+    }
+    printf("\n");
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_INFO, "Q: ");
+    for (size_t i = 0; i < private_key.q.len; i++) {
+      printf("%02x ", private_key.q.value[i]);
+    }
+    printf("\n");
+  } else {
+    atlogger_log("test_rsa_generate", ATLOGGER_LOGGING_LEVEL_ERROR, "Private key is not populated\n");
+    goto exit;
+  }
+
 exit: {
   atchops_rsa_key_public_key_free(&public_key);
   atchops_rsa_key_private_key_free(&private_key);
