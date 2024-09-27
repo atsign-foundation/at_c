82ed1667f95c533d3e9bc11c101d04bd49274dae
xavierchanth <xchanthavong@gmail.com>
2024-09-27 10:16:11 -0400

    feat: add apkam_symmetric_key to atkeys_file

--- packages/atclient/include/atclient/atkeys_file.h
+++ packages/atclient/include/atclient/atkeys_file.h
@@ -13,6 +13,7 @@
 #define ATCLIENT_ATKEYS_FILE_AES_ENCRYPT_PRIVATE_KEY_STR_INDEX 0
 #define ATCLIENT_ATKEYS_FILE_SELF_ENCRYPTION_KEY_STR_INDEX 0
 #define ATCLIENT_ATKEYS_FILE_ENROLLMENT_ID_STR_INDEX 0
+#define ATCLIENT_ATKEYS_FILE_APKAM_SYMMETRIC_KEY_STR_INDEX 0
 
 #define ATCLIENT_ATKEYS_FILE_AES_PKAM_PUBLIC_KEY_STR_INITIALIZED (VALUE_INITIALIZED << 0)
 #define ATCLIENT_ATKEYS_FILE_AES_PKAM_PRIVATE_KEY_STR_INITIALIZED (VALUE_INITIALIZED << 1)
@@ -20,8 +21,18 @@
 #define ATCLIENT_ATKEYS_FILE_AES_ENCRYPT_PRIVATE_KEY_STR_INITIALIZED (VALUE_INITIALIZED << 3)
 #define ATCLIENT_ATKEYS_FILE_SELF_ENCRYPTION_KEY_STR_INITIALIZED (VALUE_INITIALIZED << 4)
 #define ATCLIENT_ATKEYS_FILE_ENROLLMENT_ID_STR_INITIALIZED (VALUE_INITIALIZED << 5)
+#define ATCLIENT_ATKEYS_FILE_APKAM_SYMMETRIC_KEY_STR_INITIALIZED (VALUE_INITIALIZED << 6)
+
+#define ATCLIENT_ATKEYS_FILE_APKAM_PUBLIC_KEY_JSON_KEY "aesPkamPublicKey"
+#define ATCLIENT_ATKEYS_FILE_APKAM_PRIVATE_KEY_JSON_KEY "aesPkamPrivateKey"
+#define ATCLIENT_ATKEYS_FILE_DEFAULT_ENCRYPTION_PUBLIC_KEY_JSON_KEY "aesEncryptPublicKey"
+#define ATCLIENT_ATKEYS_FILE_DEFAULT_ENCRYPTION_PRIVATE_KEY_JSON_KEY "aesEncryptPrivateKey"
+#define ATCLIENT_ATKEYS_FILE_DEFAULT_SELF_ENCRYPTION_KEY_JSON_KEY "selfEncryptionKey"
+#define ATCLIENT_ATKEYS_FILE_APKAM_SYMMETRIC_KEY_JSON_KEY "apkamSymmetricKey"
+#define ATCLIENT_ATKEYS_FILE_APKAM_ENROLLMENT_ID_JSON_KEY "enrollmentId"
 
 typedef struct atclient_atkeys_file {
+  // note: `aes_` prefix means the field is encrypted with aes, not that the type of key is aes
   char *aes_pkam_public_key_str;     // encrypted with self encryption key. AES decryption with self encryption key will
                                      // reveal base64-encoded RSA key
   char *aes_pkam_private_key_str;    // encrypted with self encryption key. AES decryption with self encryption key will
@@ -32,6 +43,7 @@ typedef struct atclient_atkeys_file {
                                      // reveal base64-encoded RSA key
   char *self_encryption_key_str;     // base64-encoded non-encrypted self encryption key. base64 decoding will reveal
                                      // 32-byte AES key
+  char *apkam_symmetric_key_str;
   char *enrollment_id_str;
   uint8_t _initialized_fields[1];
 } atclient_atkeys_file;
@@ -75,13 +87,28 @@ bool atclient_atkeys_file_is_aes_pkam_private_key_str_initialized(atclient_atkey
 bool atclient_atkeys_file_is_aes_encrypt_public_key_str_initialized(atclient_atkeys_file *atkeys_file);
 bool atclient_atkeys_file_is_aes_encrypt_private_key_str_initialized(atclient_atkeys_file *atkeys_file);
 bool atclient_atkeys_file_is_self_encryption_key_str_initialized(atclient_atkeys_file *atkeys_file);
+bool atclient_atkeys_file_is_apkam_symmetric_key_str_initialized(atclient_atkeys_file *atkeys_file);
 bool atclient_atkeys_file_is_enrollment_id_str_initialized(atclient_atkeys_file *atkeys_file);
 
-int atclient_atkeys_file_set_aes_pkam_public_key_str(atclient_atkeys_file *atkeys_file, const char *aes_pkam_public_key_str, const size_t aes_pkam_public_key_str_len);
-int atclient_atkeys_file_set_aes_pkam_private_key_str(atclient_atkeys_file *atkeys_file, const char *aes_pkam_private_key_str, const size_t aes_pkam_private_key_str_len);
-int atclient_atkeys_file_set_aes_encrypt_public_key_str(atclient_atkeys_file *atkeys_file, const char *aes_encrypt_public_key_str, const size_t aes_encrypt_public_key_str_len);
-int atclient_atkeys_file_set_aes_encrypt_private_key_str(atclient_atkeys_file *atkeys_file, const char *aes_encrypt_private_key_str, const size_t aes_encrypt_private_key_str_len);
-int atclient_atkeys_file_set_self_encryption_key_str(atclient_atkeys_file *atkeys_file, const char *self_encryption_key_str, const size_t self_encryption_key_str_len);
-int atclient_atkeys_file_set_enrollment_id_str(atclient_atkeys_file *atkeys_file, const char *enrollment_id_str, const size_t enrollment_id_str_len);
+int atclient_atkeys_file_set_aes_pkam_public_key_str(atclient_atkeys_file *atkeys_file,
+                                                     const char *aes_pkam_public_key_str,
+                                                     const size_t aes_pkam_public_key_str_len);
+int atclient_atkeys_file_set_aes_pkam_private_key_str(atclient_atkeys_file *atkeys_file,
+                                                      const char *aes_pkam_private_key_str,
+                                                      const size_t aes_pkam_private_key_str_len);
+int atclient_atkeys_file_set_aes_encrypt_public_key_str(atclient_atkeys_file *atkeys_file,
+                                                        const char *aes_encrypt_public_key_str,
+                                                        const size_t aes_encrypt_public_key_str_len);
+int atclient_atkeys_file_set_aes_encrypt_private_key_str(atclient_atkeys_file *atkeys_file,
+                                                         const char *aes_encrypt_private_key_str,
+                                                         const size_t aes_encrypt_private_key_str_len);
+int atclient_atkeys_file_set_self_encryption_key_str(atclient_atkeys_file *atkeys_file,
+                                                     const char *self_encryption_key_str,
+                                                     const size_t self_encryption_key_str_len);
+int atclient_atkeys_file_set_apkam_symmetric_key_str(atclient_atkeys_file *atkeys_file,
+                                                     const char *apkam_symmetric_key_str,
+                                                     const size_t apkam_symmetric_key_str_len);
+int atclient_atkeys_file_set_enrollment_id_str(atclient_atkeys_file *atkeys_file, const char *enrollment_id_str,
+                                               const size_t enrollment_id_str_len);
 
 #endif
