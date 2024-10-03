f786bc712d933bc6f48084a85fe023058ec0dcd1
Sri Teja T <srieteja@atsign.com>
2024-10-04 02:56:15 +0530

    feat: introduce arg parser for at_activate

---
+++ packages/atauth/include/atauth/atactivate_arg_parser.h
@@ -0,0 +1,6 @@
+#ifndef ATACTIVATE_ARG_PARSER_H
+#define ATACTIVATE_ARG_PARSER_H
+
+int atactivate_parse_args(int argc, char *argv[], char *atsign, char* cram_secret, char* root_host, char *root_port);
+
+#endif //ATACTIVATE_ARG_PARSER_H
