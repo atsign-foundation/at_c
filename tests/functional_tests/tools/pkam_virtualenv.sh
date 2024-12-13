84518cd689e2fe634d32e7724e58ce453428134e
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-12-13 17:47:24 +0000

    feat: Almost working

--- tests/functional_tests/tools/pkam_virtualenv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
-SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
-cd "$SCRIPT_DIRECTORY"
-
-CONTAINER_NAME="virtualenv"
-
-sudo docker exec "$CONTAINER_NAME" supervisorctl start pkamLoad
--- tests/functional_tests/tools/start_virtualenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
-SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
-cd "$SCRIPT_DIRECTORY"
-
-docker-compose up -d
