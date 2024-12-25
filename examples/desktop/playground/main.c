696e6ade92d1cd56086aa301bc937157d05a3236
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-12-25 03:26:09 +0000

    chore: undo playground

--- examples/desktop/playground/main.c
+++ examples/desktop/playground/main.c
@@ -1,6 +1,4 @@
 #include <atlogger/atlogger.h>
-#include <string.h>
-#include <atclient/connection.h>
 
 #define TAG "main"
 
@@ -10,25 +8,6 @@ int main() {
   atlogger_set_logging_level(ATLOGGER_LOGGING_LEVEL_DEBUG);
 
   // Enter your code here
-
-  const char *host = "vip.ve.atsign.zone";
-  const int port = 64;
-
-  unsigned char recv[1024];
-  size_t recv_size = sizeof(recv);
-  size_t recv_len = 0;
-
-  atclient_connection connection;
-  atclient_connection_init(&connection, ATCLIENT_CONNECTION_TYPE_ATSERVER);
-
-  if((ret = atclient_connection_connect(&connection, host, port)) != 0) {
-    atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_ERROR, "atclient_connection_connect failed with exit code: %d\n", ret);
-    goto exit;
-  }
-
-  atlogger_log(TAG, ATLOGGER_LOGGING_LEVEL_INFO, "Connected to %s:%d\n", host, port);
-
-  atclient_connection_send(&connection, (unsigned char *)"bob🛠\n", strlen("bob🛠\n"), recv, recv_size, &recv_len);
 
   ret = 0;
 
