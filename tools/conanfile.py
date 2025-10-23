f922a9e4b2197a70502794ff03dfe5b90c7177c1
Chris Swan <478926+cpswan@users.noreply.github.com>
2025-10-23 15:22:05 +0100

    build(deps): Bump Mbed-TLS to 3.6.5

--- tools/conanfile.py
+++ tools/conanfile.py
@@ -8,7 +8,7 @@ from conan.tools.files import get
 
 class at_cRecipe(ConanFile):
     name = "at_c"
-    version = "0.3.6"
+    version = "0.3.7"
     package_type = "library"
 
     # Optional metadata
@@ -47,5 +47,5 @@ class at_cRecipe(ConanFile):
         self.cpp_info.libs = ["at_c"]
 
     def requirements(self):
-        self.requires("mbedtls/3.6.4")
+        self.requires("mbedtls/3.6.5")
         self.requires("cjson/1.7.19")
